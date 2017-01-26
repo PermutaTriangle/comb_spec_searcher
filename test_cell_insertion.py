1aaa6bf29aa1ffcaa6923283d547c31a43ef7395
RagnarArdal <RagnarArdal@gmail.com>
2017-01-26 14:00:40 +0000

    Just made the output of the test a little nicer

--- test_cell_insertion.py
+++ test_cell_insertion.py
@@ -1,5 +1,6 @@
 from atrap import *
 from permuta import *
+import sys
 
 
 #input_set = Tile.INCREASING
@@ -9,9 +10,9 @@ from permuta import *
 #input_set = PermSet.avoiding([Perm((0, 2, 1)), Perm((2, 0, 1))])
 #input_set = PermSet.avoiding([Perm((1, 2, 0))])
 #input_set = PermSet.avoiding([Perm((0, 1, 2)), Perm((1, 2, 0))])
-#input_set = PermSet.avoiding([Perm((0, 1, 2)), Perm((0, 2, 1)), Perm((1, 0, 2))])
+input_set = PermSet.avoiding([Perm((0, 1, 2)), Perm((0, 2, 1)), Perm((1, 0, 2))])
 #input_set = PermSet.avoiding([Perm((0, 1, 2)), Perm((0, 2, 1))])
-input_set = PermSet.avoiding([Perm((0, 1, 2)), Perm((2, 1, 0))])
+#input_set = PermSet.avoiding([Perm((0, 1, 2)), Perm((2, 1, 0))])
 
 T = Tiling({(0, 0): input_set})
 
@@ -54,56 +55,54 @@ for (_, left), (letter, right) in all_cell_insertions(T, input_set):
     print()
     print("Verifying left one")
     if verify_tiling(left, input_set):
-        print("Tile is verified")
+        print("Tiling is verified")
     else:
-        print("Tile is NOT verified")
-        print()
-        print("GOING FOR ROUND TWO!")
-        print()
-        for (_, left), (letter, right2) in all_cell_insertions(right, input_set):
-            print()
-            print(left)
-            print()
-            print(letter)
-            print()
-            print(right2)
-            print()
-            print("Verifying left one")
-            if verify_tiling(left, input_set):
-                print("Tile is verified")
-            else:
-                print("Tile is NOT verified")
-            print()
-            print("Verifying right2 one")
-            if verify_tiling(right2, input_set):
-                print("Tile is verified")
-            else:
-                print("Tile is NOT verified")
+        print("Tiling is NOT verified!")
+        print("This should never happen with cell insertion")
     print()
     print("Verifying right one")
     if verify_tiling(right, input_set):
-        print("Tile is verified")
+        print("Tiling is verified")
+        print()
+        print("THE VERIFIED TILING SET")
+        print()
+        print(left)
+        print()
+        print(right)
     else:
-        print("Tile is NOT verified")
+        print("Tiling is NOT verified")
         print()
         print("GOING FOR ROUND TWO!")
         print()
-        for (_, left), (letter, right2) in all_cell_insertions(right, input_set):
+        for (_, left2), (letter, right2) in all_cell_insertions(right, input_set):
             print()
-            print(left)
+            print(left2)
             print()
             print(letter)
             print()
             print(right2)
             print()
-            print("Verifying left one")
-            if verify_tiling(left, input_set):
-                print("Tile is verified")
+            print("Verifying left2 one")
+            left_verified = verify_tiling(left2, input_set)
+            if left_verified:
+                print("Tiling is verified")
             else:
-                print("Tile is NOT verified")
+                print("Tiling is NOT verified")
             print()
             print("Verifying right2 one")
-            if verify_tiling(right2, input_set):
-                print("Tile is verified")
+            right2_verified = verify_tiling(right2, input_set)
+            if right2_verified:
+                print("Tiling is verified")
             else:
-                print("Tile is NOT verified")
+                print("Tiling is NOT verified")
+            if left_verified and right2_verified:
+                # Found verification on level two, stop
+                print()
+                print("THE VERIFIED TILING SET")
+                print()
+                print(left)
+                print()
+                print(left2)
+                print()
+                print(right2)
+                #sys.exit()
