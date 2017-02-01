d1c868221c4a3801d15d816b891e59ae90bc8eda
Christian Bean <christianbean@ru.is>
2017-02-01 18:06:52 +0000

    naive reversibly deletable
    checks if a cell is reversibly deletable in a tiling with respect to a
    basis

--- atrap/recursion.py
+++ atrap/recursion.py
@@ -31,3 +31,33 @@ def components(tiling, basis):
         else:
             all_components[i] = [cell]
     return list( all_components.values() )
+
+def is_reversibly_deletable(tiling, cell, basis):
+
+    tilingpermset = PermSetTiled(tiling)
+
+    perms_to_consider = {}
+
+    for patt in basis:
+        max_length = tiling.total_points + len(patt)
+        for perm_length in range(max_length + 1):
+            # where tells you which cell a point belongs to.
+            for perm, where in tilingpermset.of_length_with_positions(perm_length):
+                occurences = [ [ perm[i] for i in occurence ] for occurence in perm.occurrences_of(patt) ]
+                if len(occurences) > 0:
+                    perms_to_consider[perm] = (occurences, where)
+
+
+    for perm, (occurences, where) in perms_to_consider.items():
+        new_occurences = []
+        for occurence in occurences:
+            if any( where[i] == cell for i in occurence ):
+                continue
+            new_occurences.append(occurence)
+        if len(new_occurences) > 0:
+            continue
+            # for continuing on in future tiling without the cell, we would go into new_perm, new_occurences and check.
+            # new_perm = [ i for i in perm if not where[i] == cell ]
+        else:
+            return False
+    return True
