9301f981f6078ec458fc26621c6967c54476ff3b
Christian Bean <christianbean@ru.is>
2017-02-02 11:32:13 +0000

    find sorted reversibly deletable paths

--- atrap/recursion.py
+++ atrap/recursion.py
@@ -4,6 +4,7 @@ from permuta.misc import UnionFind
 from itertools import combinations, chain
 from copy import copy
 
+# Overly strict version, should we just remove it?
 def components(tiling, basis):
 
     cell_to_int = {}
@@ -33,6 +34,8 @@ def components(tiling, basis):
             all_components[i] = [cell]
     return list( all_components.values() )
 
+
+# We never use this function. Probably should just delete it.
 def is_reversibly_deletable(tiling, cell, basis):
 
     tilingpermset = PermSetTiled(tiling)
@@ -46,6 +49,8 @@ def is_reversibly_deletable(tiling, cell, basis):
             for perm, where in tilingpermset.of_length_with_positions(perm_length):
                 occurences = [ [ perm[i] for i in occurence ] for occurence in perm.occurrences_of(patt) ]
                 if len(occurences) > 0:
+                    if perm in perms_to_consider:
+                        perms_to_consider[perm] = (perms_to_consider[0] + occurences, perms_to_consider[1])
                     perms_to_consider[perm] = (occurences, where)
 
 
@@ -56,8 +61,6 @@ def is_reversibly_deletable(tiling, cell, basis):
                 new_occurences.append(occurence)
         if len(new_occurences) > 0:
             continue
-            # for continuing on in future tiling without the cell, we would go into new_perm, new_occurences and check.
-            # new_perm = [ i for i in perm if not where[i] == cell ]
         else:
             return False
     return True
@@ -67,33 +70,40 @@ def reversibly_deletably_path_finder(tiling, basis):
     tilingpermset = PermSetTiled(tiling)
 
     perms_to_consider = {}
-    for patt in basis:
-        max_length = tiling.total_points + len(patt)
-        for perm_length in range(max_length + 1):
-            # where tells you which cell a point belongs to.
-            for perm, where in tilingpermset.of_length_with_positions(perm_length):
-                occurences = [ [ perm[i] for i in occurence ] for occurence in perm.occurrences_of(patt) ]
-                if len(occurences) > 0:
-                    perms_to_consider[perm] = (occurences, where)
+    max_patt_length = max(len(patt) for patt in basis)
+    max_length = tiling.total_points + max_patt_length
 
-    return list( chain( list( reversibly_deletably_path_finder_helper( cell, perms_to_consider, tiling ) ) for cell in tiling ) )
+    for perm_length in range(max_length + 1):
+        # where tells you which cell a point belongs to.
+        for perm, where in tilingpermset.of_length_with_positions(perm_length):
+            for patt in basis:
+                if max_length - tiling.total_points >= max_patt_length:
+                    occurences = [ [ where[ perm[i] ] for i in occurence ] for occurence in perm.occurrences_of(patt) ]
+                    if len(occurences) > 0:
+                        if perm in perms_to_consider:
+                            perms_to_consider[perm] += occurences
+                        else:
+                            perms_to_consider[perm] = occurences
+
+    paths = set()
+    for cell in tiling:
+        for path in reversibly_deletably_path_finder_helper( cell, perms_to_consider, tiling ):
+            yield path
 
 def reversibly_deletably_path_finder_helper(cell, perms_to_consider, tiling):
+
     new_perms_to_consider = {}
-    for perm, (occurences, where) in perms_to_consider.items():
+    for perm, occurences in perms_to_consider.items():
         new_occurences = []
         for occurence in occurences:
-            if not any( where[i] == cell for i in occurence ):
+            if not any( i == cell for i in occurence ):
                 new_occurences.append(occurence)
         if len(new_occurences) > 0:
-            new_perms_to_consider[perm] = (new_occurences, where)
+            new_perms_to_consider[perm] = new_occurences
         else:
             return
-    new_tiling = copy(tiling)
-    new_tiling.pop(cell)
     yield [cell]
-    for cell2 in new_tiling:
-        if not cell2 == cell:
-            for path in reversibly_deletably_path_finder_helper(cell2, new_perms_to_consider, new_tiling):
-                print( cell, cell2, path )
+    for cell2 in tiling:
+        if cell2 > cell:
+            for path in reversibly_deletably_path_finder_helper(cell2, new_perms_to_consider, tiling):
                 yield [cell] + path
