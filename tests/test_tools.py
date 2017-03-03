2847c19c015ffbaa05b286dec535ebc922440ac6
RagnarArdal <RagnarArdal@gmail.com>
2017-03-03 11:34:14 +0000

    Christian and I attempted to make another test, but gave up

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -109,3 +109,352 @@ def test_tiling_inferral(random_tiling_dict, random_basis):
     print(Tiling(new_tiling_dict))
     print(atrap.tools.tiling_inferral(tiling, basis))
     assert dict(Tiling(new_tiling_dict)) == dict(atrap.tools.tiling_inferral(tiling, basis))
+
+
+def test_tiling_inferral_hardcoded():
+    basis = Basis([Perm((0, 1, 2)),
+                   Perm((0, 3, 2, 1)),
+                   Perm((1, 3, 0, 2)),
+                   Perm((1, 3, 2, 0)),
+                   Perm((2, 3, 1, 0)),
+                   Perm((3, 1, 0, 2)),
+                   Perm((3, 1, 2, 0)),
+                   Perm((3, 2, 0, 1)),
+                  ])
+
+    # The permsets
+    input_set = PermSet(basis)
+    point = Block.point
+    dec = Block.decreasing
+    five = PermSet(Basis([Perm((0, 1, 2)),
+                          Perm((1, 0, 2)),
+                          Perm((1, 2, 0)),
+                          Perm((2, 0, 1)),
+                          Perm((0, 3, 2, 1)),
+                         ]))
+    poe = Block.point_or_empty
+    almost_poe = PermSet.avoiding([Perm((1, 0)), Perm((0, 1, 2))])
+
+    # The pairs of a tiling and its inferred tiling
+    pairs = [
+        [
+            Tiling({(0, 0): input_set,
+                   }),
+            Tiling({(0, 0): input_set,
+                   })
+        ],
+        [
+            Tiling({}),
+            Tiling({})
+        ],
+        [
+            Tiling({(0, 0): input_set,
+                    (1, 1): point,
+                    (2, 0): input_set,
+                   }),
+            Tiling({(0, 0): dec,
+                    (1, 1): point,
+                    (2, 0): five,
+                   }),
+        ],
+        [
+            Tiling({(0, 1): point,
+                    (1, 0): five,
+                   }),
+            Tiling({(0, 1): point,
+                    (1, 0): five,
+                   }),
+        ],
+        [
+            Tiling({(0, 0): dec,
+                    (1, 1): point,
+                    (2, 0): dec,
+                    (3, 3): point,
+                    (4, 2): five,
+                    (5, 0): five,
+                   }),
+            Tiling({(0, 1): point,
+                    (1, 0): dec,
+                    (2, 3): point,
+                    (3, 2): poe,
+                    (4, 0): almost_poe,
+                   })
+        ],
+        [
+            Tiling({(0, 4): point,
+                    (1, 1): dec,
+                    (2, 2): point,
+                    (3, 1): dec,
+                    (4, 6): point,
+                    (5, 5): poe,
+                    (6, 0): almost_poe,
+                    (7, 3): almost_poe,
+                   }),
+            Tiling({(0, 4): point,
+                    (1, 2): point,
+                    (2, 1): dec,
+                    (3, 6): point,
+                    (4, 5): poe,
+                    (5, 0): poe,
+                    (6, 3): poe,
+                   })
+        ],
+        [
+            Tiling({(0, 1): point,
+                    (1, 3): point,
+                    (2, 2): poe,
+                    (3, 0): almost_poe,
+                   }),
+            Tiling({(0, 1): point,
+                    (1, 3): point,
+                    (2, 2): poe,
+                    (3, 0): almost_poe,
+                   }),
+        ],
+        [
+            Tiling({(0, 1): point,
+                    (1, 2): point,
+                    (2, 0): almost_poe,
+                   }),
+            Tiling({(0, 1): point,
+                    (1, 2): point,
+                    (2, 0): almost_poe,
+                   }),
+        ],
+        [
+            Tiling({(0, 1): point,
+                    (1, 4): point,
+                    (2, 2): poe,
+                    (3, 3): point,
+                    (4, 2): poe,
+                    (5, 0): almost_poe,
+                   }),
+            Tiling({(0, 0): point,
+                    (1, 2): point,
+                    (2, 1): point,
+                   })
+        ],
+        [
+            Tiling({(0, 3): point,
+                    (1, 2): point,
+                    (2, 1): dec,
+                    (3, 5): point,
+                    (4, 4): poe,
+                    (5, 0): poe,
+                   }),
+            Tiling({(0, 3): point,
+                    (1, 2): point,
+                    (2, 1): dec,
+                    (3, 5): point,
+                    (4, 4): poe,
+                    (5, 0): poe,
+                   }),
+        ],
+        [
+            Tiling({(0, 5): point,
+                    (1, 2): point,
+                    (2, 1): dec,
+                    (3, 7): point,
+                    (4, 6): poe,
+                    (5, 0): poe,
+                    (6, 3): poe,
+                    (7, 4): point,
+                    (8, 3): poe,
+                   }),
+            Tiling({(0, 2): point,
+                    (1, 0): point,
+                    (2, 3): point,
+                    (3, 1): point,
+                   })
+        ],
+        [
+            Tiling({(0, 3): point,
+                    (1, 2): point,
+                    (2, 1): dec,
+                    (3, 4): point,
+                    (5, 0): poe,
+                   }),
+            Tiling({(0, 3): point,
+                    (1, 2): point,
+                    (2, 1): dec,
+                    (3, 4): point,
+                    (5, 0): poe,
+                   }),
+        ],
+        [
+            Tiling({(0, 3): point,
+                    (1, 2): point,
+                    (2, 1): dec,
+                    (3, 6): point,
+                    (4, 4): poe,
+                    (5, 5): point,
+                    (6, 4): poe,
+                    (7, 0): poe,
+                   }),
+            Tiling({(0, 2): point,
+                    (1, 1): point,
+                    (2, 0): dec,
+                    (3, 4): point,
+                    (4, 3): point,
+                   })
+        ],
+    ]
+
+    # TODO: Parameterize or something
+    for original, inferred in pairs:
+        assert atrap.tools.tiling_inferral(original, basis) == inferred
+
+
+def test_tiling_inferral_hardcoded_positive_class():
+    basis = Basis([Perm((0, 1, 2)),
+                   Perm((0, 3, 2, 1)),
+                   Perm((1, 3, 0, 2)),
+                   Perm((1, 3, 2, 0)),
+                   Perm((2, 1, 3, 0)),
+                  ])
+
+    # The permsets
+    input_set = PermSet(basis)
+    point = Block.point
+    dec = Block.decreasing
+    three = PermSet(Basis([Perm((0, 1, 2)),
+                           Perm((0, 2, 1)),
+                           Perm((1, 0, 2)),
+                          ]))
+    poe = Block.point_or_empty
+    pos_input_set = PositiveClass(input_set)
+    pos_dec = PositiveClass(dec)
+    pos_three = PositiveClass(three)
+    pos_poe = PositiveClass(poe)  # TODO: This is the point?
+
+    # The pairs of a tiling and its inferred tiling
+    pairs = [
+        [
+            Tiling({(0, 0): input_set}),
+            Tiling({(0, 0): input_set}),
+        ],
+        [
+            Tiling({}),
+            Tiling({}),
+        ],
+        [
+            Tiling({(0, 0): pos_input_set}),
+            Tiling({(0, 0): pos_input_set}),
+        ],
+        [
+            Tiling({(0, 0): input_set,
+                    (0, 2): input_set,
+                    (1, 1): point,
+                   }),
+            Tiling({(0, 0): dec,
+                    (0, 2): three,
+                    (1, 1): point,
+                   }),
+        ],
+        [
+            Tiling({(0, 0): input_set,
+                    (0, 1): input_set,
+                    (0, 2): input_set,
+                    (0, 3): input_set,
+                    (0, 4): input_set,
+                    (0, 5): input_set,
+                    (0, 6): input_set,
+                    (1, 0): input_set,
+                    (1, 1): input_set,
+                    (1, 2): input_set,
+                    (1, 3): input_set,
+                    (1, 4): input_set,
+                    (1, 5): input_set,
+                    (1, 6): input_set,
+                    (2, 0): input_set,
+                    (2, 1): input_set,
+                    (2, 2): input_set,
+                    (2, 3): input_set,
+                    (2, 4): input_set,
+                    (2, 5): input_set,
+                    (2, 6): input_set,
+                    (3, 0): input_set,
+                    (3, 1): input_set,
+                    (3, 2): input_set,
+                    (3, 3): input_set,
+                    (3, 4): input_set,
+                    (3, 5): input_set,
+                    (3, 6): input_set,
+                    (4, 0): input_set,
+                    (4, 1): input_set,
+                    (4, 2): input_set,
+                    (4, 3): input_set,
+                    (4, 4): input_set,
+                    (4, 5): input_set,
+                    (4, 6): input_set,
+                    (5, 0): input_set,
+                    (5, 1): input_set,
+                    (5, 2): input_set,
+                    (5, 3): input_set,
+                    (5, 4): input_set,
+                    (5, 5): input_set,
+                    (5, 6): input_set,
+                    (6, 0): input_set,
+                    (6, 1): input_set,
+                    (6, 2): input_set,
+                    (6, 3): input_set,
+                    (6, 4): input_set,
+                    (6, 5): input_set,
+                    (6, 6): input_set,
+                   }),
+            Tiling({(0, 0): input_set,
+                    (0, 1): input_set,
+                    (0, 2): input_set,
+                    (0, 3): input_set,
+                    (0, 4): input_set,
+                    (0, 5): input_set,
+                    (0, 6): input_set,
+                    (1, 0): input_set,
+                    (1, 1): input_set,
+                    (1, 2): input_set,
+                    (1, 3): input_set,
+                    (1, 4): input_set,
+                    (1, 5): input_set,
+                    (1, 6): input_set,
+                    (2, 0): input_set,
+                    (2, 1): input_set,
+                    (2, 2): input_set,
+                    (2, 3): input_set,
+                    (2, 4): input_set,
+                    (2, 5): input_set,
+                    (2, 6): input_set,
+                    (3, 0): input_set,
+                    (3, 1): input_set,
+                    (3, 2): input_set,
+                    (3, 3): input_set,
+                    (3, 4): input_set,
+                    (3, 5): input_set,
+                    (3, 6): input_set,
+                    (4, 0): input_set,
+                    (4, 1): input_set,
+                    (4, 2): input_set,
+                    (4, 3): input_set,
+                    (4, 4): input_set,
+                    (4, 5): input_set,
+                    (4, 6): input_set,
+                    (5, 0): input_set,
+                    (5, 1): input_set,
+                    (5, 2): input_set,
+                    (5, 3): input_set,
+                    (5, 4): input_set,
+                    (5, 5): input_set,
+                    (5, 6): input_set,
+                    (6, 0): input_set,
+                    (6, 1): input_set,
+                    (6, 2): input_set,
+                    (6, 3): input_set,
+                    (6, 4): input_set,
+                    (6, 5): input_set,
+                    (6, 6): input_set,
+                   })
+        ],
+    ]
+
+    # TODO: Parameterize or something
+    for original, inferred in pairs:
+        assert atrap.tools.tiling_inferral(original, basis) == inferred
