d7077977ab67ef6333702733e8144dd4552aa063
Christian Bean <christianbean@ru.is>
2017-04-12 19:33:32 +0000

    refactored equivalent row placement
    All functions in this file should be refactored in a similar manner.

--- atrap/strategies/equivalence_strategies/row_column_placement.py
+++ atrap/strategies/equivalence_strategies/row_column_placement.py
@@ -1,14 +1,17 @@
 from grids import Tiling, Block, PositiveClass
 
 from .equivalence_class import EquivalenceStrategy
+from .point_placement import all_unique_point_or_empty, all_maximum_point_placements, all_maximum_point_placements
 
 def all_equivalent_row_placements(tiling, **kwargs):
     for row in range(tiling.dimensions.j):
         if len(tiling.get_row(row)) != 1:
             continue
 
-        if any( not (block is Block.point or isinstance(block, PositiveClass)) for _, block in tiling.get_row(row) ):
-            # Row inelegible as some block is not a point.
+        cell, block = tiling.get_row(row)[0]
+
+        if not isinstance(block, PositiveClass) or block is Block.point:
+            # Row inelegible as it is not a PositiveClass
             continue
 
         if any( sum(1 for _, col_block in tiling.get_col(cell.i)
@@ -18,50 +21,63 @@ def all_equivalent_row_placements(tiling, **kwargs):
             # the sole non-class cell in its respective col
             continue
 
-        bottommost_tiling_dicts = [ {} for _ in tiling.get_row(row) ]
-        topmost_tiling_dicts = [ {} for _ in tiling.get_row(row) ]
-        for cell, block in tiling:
+        if block is Block.decreasing:
+            for strategy in all_minimum_and_maximum_decreasing( tiling, cell ):
+                yield strategy
+            continue
 
-            for index, value in enumerate(tiling.get_row(row)):
-                cell_being_placed, _ = value
+        if block is Block.increasing:
+            for strategy in all_minimum_and_maximum_increasing( tiling, cell ):
+                yield strategy
+            continue
 
-                if cell.i == cell_being_placed.i:
-                    if cell.j == cell_being_placed.j:
-                        # same cell
+        if block is Block.point_or_empty:
+            for strategy in all_unique_point_or_empty( tiling, cell ):
+                yield strategy
+            continue
 
-                        bottommost_tiling_dicts[index][cell] = Block.point
-                        topmost_tiling_dicts[index][cell] = Block.point
+        topmost_tiling_dict = {}
+        bottommost_tiling_dict = {}
 
-                        if isinstance(block, PositiveClass):
-                            perm_class = block.perm_class
-                            bottommost_tiling_dicts[index][(cell.i - 0.5, cell.j + 0.5)] = perm_class
-                            bottommost_tiling_dicts[index][(cell.i + 0.5, cell.j + 0.5)] = perm_class
-                            topmost_tiling_dicts[index][(cell.i + 0.5, cell.j - 0.5)] = perm_class
-                            topmost_tiling_dicts[index][(cell.i - 0.5, cell.j - 0.5)] = perm_class
-                    else:
-                        # same column, different row
-                        bottommost_tiling_dicts[index][(cell.i + 0.5, cell.j)] = block
-                        bottommost_tiling_dicts[index][(cell.i - 0.5, cell.j)] = block
-                        topmost_tiling_dicts[index][(cell.i + 0.5, cell.j)] = block
-                        topmost_tiling_dicts[index][(cell.i - 0.5, cell.j)] = block
-                elif cell.j == cell_being_placed.j:
-                    #same row, different column
-                    bottommost_tiling_dicts[index][(cell.i, cell.j + 0.5)] = block
-                    topmost_tiling_dicts[index][(cell.i, cell.j - 0.5)] = block
+        for new_cell, new_block in tiling:
+            if new_cell.i == cell.i:
+                # same cell
+                if new_cell.j == cell.j:
+                    perm_class = new_block.perm_class
+                    topmost_tiling_dict[(cell.i + 0.5, cell.j - 0.5)] = perm_class
+                    topmost_tiling_dict[(cell.i - 0.5, cell.j - 0.5)] = perm_class
+                    topmost_tiling_dict[cell] = Block.point
+
+                    bottommost_tiling_dict[(cell.i + 0.5, cell.j + 0.5)] = perm_class
+                    bottommost_tiling_dict[(cell.i - 0.5, cell.j + 0.5)] = perm_class
+                    bottommost_tiling_dict[cell] = Block.point
+
+                # same column, but different row
                 else:
-                    # different row and different column
-                    bottommost_tiling_dicts[index][cell] = block
-                    topmost_tiling_dicts[index][cell] = block
+                    topmost_tiling_dict[(new_cell.i + 0.5, new_cell.j)] = new_block
+                    topmost_tiling_dict[(new_cell.i - 0.5, new_cell.j)] = new_block
 
-        bottommost_tilings = [ Tiling(tiling_dict) for tiling_dict in bottommost_tiling_dicts]
-        assert len(bottommost_tilings) == 1
-        bottommost_formal_step = "Placing the minimum point into row {}".format(row)
-        yield EquivalenceStrategy( bottommost_formal_step, bottommost_tilings[0] )
+                    bottommost_tiling_dict[(new_cell.i + 0.5, new_cell.j)] = new_block
+                    bottommost_tiling_dict[(new_cell.i - 0.5, new_cell.j)] = new_block
 
-        topmost_tilings = [ Tiling(tiling_dict) for tiling_dict in topmost_tiling_dicts ]
-        assert len(topmost_tilings) == 1
-        topmost_formal_step = "Placing the maximum point into row {}".format(row)
-        yield EquivalenceStrategy( topmost_formal_step, topmost_tilings[0] )
+            # same row, but different column
+            elif new_cell.j == cell.j:
+                topmost_tiling_dict[(new_cell.i, new_cell.j + 0.5)] = new_block
+                topmost_tiling_dict[(new_cell.i, new_cell.j - 0.5)] = new_block
+
+                bottommost_tiling_dict[(new_cell.i, new_cell.j + 0.5)] = new_block
+                bottommost_tiling_dict[(new_cell.i, new_cell.j - 0.5)] = new_block
+
+            # different row and column
+            else:
+                topmost_tiling_dict[new_cell] = new_block
+                bottommost_tiling_dict[new_cell] = new_block
+
+        topmost_tiling = Tiling(topmost_tiling_dict)
+        bottommost_tiling = Tiling(bottommost_tiling_dict)
+
+        yield EquivalenceStrategy( "Inserting the top most point in to the cell " + str(cell), topmost_tiling )
+        yield EquivalenceStrategy( "Inserting the bottom most point in to the cell " + str(cell), bottommost_tiling )
 
 def all_equivalent_minimum_row_placements(tiling, **kwargs):
     for row in range(tiling.dimensions.j):
