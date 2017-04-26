cd76572939079ce3d52b75e76e9fe67595d50da7
Henning Ulfarsson <ulfarsson@gmail.com>
2017-04-26 13:21:25 +0000

    Getting ready to run the non-regular insertion encodable bases

--- non-regular-insertion-encodable-bases/run.py
+++ non-regular-insertion-encodable-bases/run.py
@@ -10,17 +10,17 @@ all_strategies = [ [all_cell_insertions, all_row_placements], [all_equivalent_ro
 
 mimic_regular_insertion_encoding = [ [all_cell_insertions, all_minimum_row_placements], [all_equivalent_minimum_row_placements], [empty_cell_inferral], [reversibly_deletable_cells], [one_by_one_verification, is_empty]]
 
-# Run 1
-standard_strategies = [ [all_active_cell_insertions], [all_point_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
-# standard_strategies = [ [all_cell_insertions], [all_point_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
-standard_strategies = [ [all_cell_insertions, all_row_placements, all_column_placements], [all_equivalent_row_placements, all_equivalent_column_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
+standard_strategies_w_left_col = [ [all_cell_insertions, all_leftmost_column_placements], [all_equivalent_leftmost_column_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
+standard_strategies_w_min_row = [ [all_cell_insertions, all_minimum_row_placements], [all_equivalent_minimum_row_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
+standard_strategies_w_left_col_min_row = [ [all_cell_insertions, all_leftmost_column_placements, all_minimum_row_placements], [all_equivalent_leftmost_column_placements, all_equivalent_minimum_row_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
+standard_strategies_w_all_cols = [ [all_cell_insertions, all_column_placements], [all_equivalent_column_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
+standard_strategies_w_all_rows = [ [all_cell_insertions, all_row_placements], [all_equivalent_row_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
+standard_strategies_w_all_rows_cols = [ [all_cell_insertions, all_row_placements, all_column_placements], [all_equivalent_row_placements, all_equivalent_column_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
+standard_strategies_point_pl = [ [all_cell_insertions], [all_point_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral,], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
 
-# Run 2
-# standard_strategies = [ [all_cell_insertions], [all_point_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [components, reversibly_deletable_cells], [subset_verified, is_empty] ]
-
-# Next runs: Add symmetries, splittings, fission/fusion
-
-finite_strategies = [ [all_cell_insertions, all_minimum_row_placements], [all_equivalent_minimum_row_placements], [empty_cell_inferral, subclass_inferral], [], [subset_verified] ]
+# First one is best
+finite_strategies_w_min_row = [ [all_cell_insertions, all_minimum_row_placements], [all_equivalent_minimum_row_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [], [subset_verified, is_empty] ]
+finite_strategies_w_max_point_pl = [ [all_cell_insertions], [all_maximum_point_placements], [empty_cell_inferral, row_and_column_separation, subclass_inferral], [], [subset_verified, is_empty] ]
 
 # import sys
 
@@ -32,43 +32,78 @@ def perms_to_str(perms):
     return "_".join(perm_to_str(perm) for perm in perms)
 
 length_to_process = '11'
+strat_pack = standard_strategies_w_all_cols
+strat_pack_desc = 'standard_strategies_w_all_cols'
+max_depth = 8
 
-with open('length'+length_to_process) as f:
-# with open('length'+length_to_process+'_depth_geq4') as f:
-    content = f.readlines()
+first_failure = False
+first_success = False
+
+# with open('length'+length_to_process) as g:
+# with open('test') as f:
+with open('length'+length_to_process+'_failed') as g:
+    content = g.readlines()
     content = [x.strip() for x in content]
+
+    for i in range(len(content)):
+        print(i)
+        print(content[len(content)-1-i])
+        print(content[len(content)-1-i].split())
+        if content[len(content)-1-i].split() and content[len(content)-1-i].split()[0] == 'Maximum':
+            print('Breaking')
+            print(i)
+            i = i-2
+            print(content[len(content)-1-i])
+
+            break
+
+    print(len(content))
+    content = content[len(content)-1-i::]
+    print(content)
+
     for task in content:
+
+        if task == '':
+            continue
 
         print('Now processing {}'.format(task))
 
         patts = [ Perm([ int(c) for c in p ]) for p in task.split('_') ]
-        mtree = MetaTree(patts, *standard_strategies)
+        mtree = MetaTree(patts, *strat_pack)
 
         with open(task, "w") as f:
 
             print(task, file=f)
-            print('')
             print("Log created ", strftime("%a, %d %b %Y %H:%M:%S", gmtime()),file=f)
 
             start_time = time()
+            depth_tried = 0
 
             while not mtree.has_proof_tree():
+
+                if mtree.depth_searched == max_depth:
+                    break
+
                 mtree.do_level()
+                depth_tried += 1
+
                 print("We had {} inferral cache hits and {} partitioning cache hits".format(mtree.inferral_cache_hits, mtree.partitioning_cache_hits))
                 print("The partitioning cache has {} tilings in it right now".format( len(mtree._basis_partitioning_cache) ) )
                 print("The inferral cache has {} tilings in it right now".format( len(mtree._inferral_cache) ) )
                 print("There are {} tilings in the search tree".format( len(mtree.tiling_cache)))
                 print("Time taken so far is {} seconds".format( time() - start_time ) )
                 print('')
-                if mtree.depth_searched == 6:
-                    break
 
             end_time = time()
 
             if mtree.has_proof_tree():
                 proof_tree = mtree.find_proof_tree()
 
-                print("Finding the proof tree for", task ,  "took", int(end_time - start_time), "seconds", file=f)
+                print("Finding the proof tree took", int(end_time - start_time), "seconds", file=f)
+                print('The depth searched was {}'.format(depth_tried), file=f)
+                print("",file=f)
+                print('Strategies applied: {}'.format(strat_pack_desc), file=f)
+                print('Maximum depth set at {}'.format(str(max_depth)), file=f)
                 print("",file=f)
                 print("Human readable:", file=f)
                 print("",file=f)
@@ -79,6 +114,55 @@ with open('length'+length_to_process) as f:
                 print("Computer readable (JSON):", file=f)
                 print("", file=f)
                 print(proof_tree.to_json(sort_keys=True), file=f)
+
+                if not first_success:
+                    first_success = True
+                    with open('length{}_succeeded'.format(length_to_process), 'a+') as f:
+                        print('', file=f)
+                        print('Strategies applied: {}'.format(strat_pack_desc), file=f)
+                        print('Maximum depth set at {}'.format(str(max_depth)), file=f)
+                        print('', file=f)
+
+                with open('length{}_succeeded'.format(length_to_process), 'a+') as e:
+                    print(task, file=e)
+
             else:
-                print('No proof tree found', file=f)
-                print("Took", int(end_time - start_time), "seconds", file=f)
+                print('No proof tree found. Tried for {} seconds'.format(int(end_time - start_time)), file=f)
+                print("",file=f)
+                print("",file=f)
+                print('Strategies applied: {}'.format(strat_pack_desc), file=f)
+                print('Maximum depth set at {}'.format(str(max_depth)), file=f)
+                print("",file=f)
+
+                if not first_failure:
+                    first_failure = True
+                    with open('length{}_failed'.format(length_to_process), 'a+') as f:
+                        print('', file=f)
+                        print('Strategies applied: {}'.format(strat_pack_desc), file=f)
+                        print('Maximum depth set at {}'.format(str(max_depth)), file=f)
+                        print('', file=f)
+
+                with open('length{}_failed'.format(length_to_process), 'a+') as e:
+                    print(task, file=e)
+
+if not first_failure:
+    with open('length{}_failed'.format(length_to_process), 'a+') as f:
+        print('', file=f)
+        print('Strategies applied: {}'.format(strat_pack_desc), file=f)
+        print('Maximum depth set at {}'.format(str(max_depth)), file=f)
+        print('', file=f)
+
+else:
+    with open('length{}_failed'.format(length_to_process), 'a+') as f:
+        print('', file=f)
+
+if not first_success:
+    with open('length{}_succeeded'.format(length_to_process), 'a+') as f:
+        print('', file=f)
+        print('Strategies applied: {}'.format(strat_pack_desc), file=f)
+        print('Maximum depth set at {}'.format(str(max_depth)), file=f)
+        print('', file=f)
+
+else:
+    with open('length{}_succeeded'.format(length_to_process), 'a+') as f:
+        print('', file=f)
