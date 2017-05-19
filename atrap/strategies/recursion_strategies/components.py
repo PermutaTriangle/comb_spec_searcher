53fcc183a25f5299f78b5b29a3a28c655bd370a7
Henning Ulfarsson <ulfarsson@gmail.com>
2017-05-19 08:03:59 +0000

    Updates to the 2x4 overview

--- atrap/strategies/recursion_strategies/components.py
+++ atrap/strategies/recursion_strategies/components.py
@@ -6,30 +6,30 @@ from atrap.tools import cells_of_occurrences
 
 from .recursive_class import RecursiveStrategy
 
-def components(tiling, basis, basis_partitioning=None,  **kwargs):
+def components(tiling, basis, basis_partitioning=None, **kwargs):
 
     cell_to_int = {}
 
-    for cell,_ in tiling:
+    for cell, _ in tiling:
         # TODO: use integer mapping
         cell_to_int[cell] = len(cell_to_int)
 
-    components = UnionFind(len(cell_to_int))
+    components_set = UnionFind(len(cell_to_int))
 
     occurrences_of_basis_elements = cells_of_occurrences(tiling, basis, basis_partitioning=basis_partitioning)
     for cells_of_occurrence in occurrences_of_basis_elements:
         for cell1, cell2 in combinations(cells_of_occurrence, 2):
-            components.unite(cell_to_int[cell1], cell_to_int[cell2])
+            components_set.unite(cell_to_int[cell1], cell_to_int[cell2])
 
 
     all_components = {}
     for cell, _ in tiling:
-        i = components.find( cell_to_int[cell] )
+        i = components_set.find(cell_to_int[cell])
         if i in all_components:
             all_components[i].append(cell)
         else:
             all_components[i] = [cell]
-    cells_of_new_tilings = list( all_components.values() )
+    cells_of_new_tilings = list(all_components.values())
 
     if len(cells_of_new_tilings) == 1:
         return
@@ -43,5 +43,5 @@ def components(tiling, basis, basis_partitioning=None,  **kwargs):
     if len(strategy) <= 1:
         return
 
-    yield RecursiveStrategy( "The components of the tiling", strategy, [tiling._back_map for tiling in strategy]  )
+    yield RecursiveStrategy("The components of the tiling", strategy, [tiling._back_map for tiling in strategy])
 # Consider the union of components?
