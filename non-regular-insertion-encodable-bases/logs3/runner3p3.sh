462f5c8162fb5ce4580160008c5dd1a3ff6e69e8
Henning Úlfarsson <henningu@garpur.localdomain>
2017-05-25 11:13:11 +0000

    3x4 logs partially finished

---
+++ non-regular-insertion-encodable-bases/logs3/runner3p3.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+#SBATCH --job-name=NRIE3p3
+
+module load python/3.6.1
+
+source ~/permenv/bin/activate
+
+export PYTHONPATH='/users/home/henningu/lib/python3.6/site-packages/'
+
+pwd
+
+python3 run_batchp3.py
