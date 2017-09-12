011892571bee7ae637b97af8d18c2d77950f904e
Tomas Ken <tomkenmag@gmail.com>
2017-09-12 11:00:30 +0000

    Updated and cleaned binary pattern strategies for using all patterns

--- atrapv2/strategies/batch_strategies/binary_pattern_strategies/binary_pattern.py
+++ atrapv2/strategies/batch_strategies/binary_pattern_strategies/binary_pattern.py
@@ -7,10 +7,16 @@ from ..batch_class import BatchStrategy
 from atrapv2.strategies import Strategy
 from itertools import chain
 from .util import *
-from .coincidence_classification import coincclass012, coincclass021
+from .coincidence_classification import *
 
-coincidence_classes = {Perm((0,1,2)) : coincclass012,
-                       Perm((0, 2, 1)) : coincclass021}
+coincidence_classification = {
+    Perm((0, 1, 2)) : coincclass012,
+    Perm((0, 2, 1)) : coincclass021,
+    Perm((1, 0, 2)) : coincclass102,
+    Perm((1, 2, 0)) : coincclass120,
+    Perm((2, 0, 1)) : coincclass201,
+    Perm((2, 1, 0)) : coincclass210
+}
 
 def binary_pattern(tiling, basis, **kwargs):
     """Produces a binary pattern strategy from a tiling containing a single
@@ -31,27 +37,14 @@ def binary_pattern(tiling, basis, **kwargs):
     if isinstance(block, PositiveClass) and k:
         for patt in PermSet.avoiding(basis).of_length(k):
             print(patt, file=sys.stderr)
-            if patt != Perm((0, 2, 1)) and patt != Perm((0, 1, 2)):
+            if patt not in coincidence_classification:
                 continue
             inferred_patt = infer_empty_boxes(patt, basis)
             inferred_patt_bin = shad_to_binary(inferred_patt.shading, len(inferred_patt) + 1)
-            print(inferred_patt, file=sys.stderr)
-            print(inferred_patt_bin, file=sys.stderr)
-            cclass = chain.from_iterable(clas for clas in coincidence_classes[patt] if any(is_subset(c, inferred_patt_bin) for c in clas))
+            # print(inferred_patt, file=sys.stderr)
+            # print(inferred_patt_bin, file=sys.stderr)
+            cclass = chain.from_iterable(clas for clas in coincidence_classification[patt] if any(is_subset(c, inferred_patt_bin) for c in clas))
             maxibin = filter(lambda x: is_binary(MeshPatt.unrank(patt, x), basis), filter_maximal(cclass))
-            # maxibin = list(filter(lambda x: is_binary(x, basis), filter_maximal(cclass)))
-            # print(list(cclass))
-
-            # cclass = list(map(lambda m: MeshPatt.unrank(patt, m), coincidence_classification[patt]))
-            # maximal = list()
-            # last = None
-
-            # maxibin = filter(lambda x: is_binary(x, basis), maximal)
-
-            # When printing out, the lazy iterator has to materialized
-            # maxibin = list(filter(lambda x: is_binary(x, basis), maximal))
-            # print("Length of maximal: ", len(maxibin), file=sys.stderr)
-            # continue
 
             for mpatt_bin in maxibin:
                 # print(None, file=sys.stderr)
@@ -62,5 +55,3 @@ def binary_pattern(tiling, basis, **kwargs):
                         tiling_from_mesh_pattern(mpatt, block.perm_class)]
                 yield Strategy(("Placing the binary pattern "
                                      "{}").format(mpatt), tilings, [False, True])
-                break
-            break
