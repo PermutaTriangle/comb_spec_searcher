c49507e70ba651710ab1d0faf5c52fc09e5d0fac
Tomas Ken <tomkenmag@gmail.com>
2017-10-05 20:19:08 +0000

    Added strategy pack for binary force strategy

--- tilescopetwo/strategy_packs.py
+++ tilescopetwo/strategy_packs.py
@@ -1,5 +1,6 @@
 from tilescopetwo.strategies import *
 from comb_spec_searcher import StrategyPack
+from functools import partial
 
 point_placement_with_subobstruction_inferral = StrategyPack(
          eq_strats=[all_point_placements],
@@ -21,3 +22,19 @@ point_placement_no_infer = StrategyPack(
          inf_strats=[],
          other_strats=[[components], [all_cell_insertions]],
          name="point_placement_no_infer")
+
+# binary_force = StrategyPack(
+    # eq_strats=[all_point_placements],
+    # ver_strats=[subset_verified],
+    # inf_strats=[subobstruction_inferral],
+    # other_strats=[[components], [all_cell_insertions],
+                  # [partial(forced_binary_pattern, maxlen=3, forcelen=2)]],
+    # name="binary_force")
+
+binary_force = StrategyPack(
+    eq_strats=[all_point_placements],
+    ver_strats=[subset_verified],
+    inf_strats=[subobstruction_inferral],
+    other_strats=[[components], [all_cell_insertions],
+                  [forced_binary_pattern]],
+    name="binary_force")
