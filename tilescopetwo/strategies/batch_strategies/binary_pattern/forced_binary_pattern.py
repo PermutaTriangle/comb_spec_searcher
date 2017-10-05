829eb7c1f76ee91ed56119a10b36028a66b9b415
Tomas Ken <tomkenmag@gmail.com>
2017-10-05 16:24:41 +0000

    Forced binary pattern refactored, optimized and cleaned up

--- tilescopetwo/strategies/batch_strategies/binary_pattern/forced_binary_pattern.py
+++ tilescopetwo/strategies/batch_strategies/binary_pattern/forced_binary_pattern.py
@@ -31,188 +31,15 @@ def translate(insert, cell):
 def translate_set(insert, cells):
     res = []
     for cell in cells:
+        translated = translate(insert, cell)
         if cell[0] == insert[0] and cell[1] == insert[1]:
-            res.append((cell[0] + 2, cell[1] + 2))
+            res.append((translated[0] + 2, translated[1] + 2))
         if cell[0] == insert[0]:
-            res.append((cell[0] + 2, cell[1]))
+            res.append((translated[0] + 2, translated[1]))
         if cell[1] == insert[1]:
-            res.append((cell[0], cell[1] + 2))
-        res.append(translate(insert, cell))
+            res.append((translated[0], translated[1] + 2))
+        res.append(translated)
     return res
-
-
-def place_forced_pattern_failed(tiling, cell, patt, force):
-    workobs = [Obstruction.single_cell(patt, cell)]
-    worktiling = tiling
-    insertedind, insertedval = list(), list()
-    for (index, dire) in force:
-        # print(worktiling)
-        insert_cell = (bisect(insertedind, index) * 2,
-                       bisect(insertedval, patt[index]) * 2)
-        insertedind.append(index)
-        insertedval.append(patt[index])
-        obs = list(chain.from_iterable(ob.place_point(insert_cell, DIR_NONE)
-                                       for ob in worktiling.obstructions))
-
-        fromworkobs = list(chain.from_iterable(
-            ob.place_point(insert_cell, opposite_dir(dire)) for ob in workobs))
-
-        # print("OBS")
-        # for ob in obs:
-            # print(ob)
-
-        # print("FROMWORKOBS")
-        # for ob in fromworkobs:
-            # print(ob)
-
-        workobs = list(ob for ob in fromworkobs
-                       if len(ob) < len(patt) - len(insertedind) + 1)
-
-        # print("WORKOBS")
-        # for ob in workobs:
-            # print(ob)
-
-        addedobs = list(ob for ob in fromworkobs
-                        if len(ob) == len(patt) - len(insertedind) + 1)
-
-        # print("ADDEDOBS")
-        # for ob in addedobs:
-            # print(ob)
-
-        # print("INSERT_CELL:", insert_cell)
-
-        # print(translate_set(insert_cell, worktiling.possibly_empty))
-
-        worktiling = Tiling(
-            point_cells=(translate_set(insert_cell, worktiling.point_cells) +
-                         [(insert_cell[0] + 1, insert_cell[1] + 1)]),
-            positive_cells=translate_set(insert_cell,
-                                         worktiling.positive_cells),
-            possibly_empty=translate_set(insert_cell,
-                                         worktiling.possibly_empty),
-            obstructions=obs + addedobs,
-            remove_empty=False)
-
-    # print(worktiling)
-    worktiling._minimize(True)
-    # print(worktiling)
-    for ob in worktiling.obstructions:
-        print(ob)
-
-    for index in range(len(patt)):
-        if index in insertedind:
-            continue
-        insert_cell = (bisect(insertedind, index) * 2,
-                       bisect(insertedval, patt[index]) * 2)
-        insertedind.append(index)
-        insertedval.append(patt[index])
-        obs = list(chain.from_iterable(ob.place_point(insert_cell, DIR_NONE)
-                                       for ob in worktiling.obstructions))
-        worktiling = Tiling(
-            point_cells=(translate_set(insert_cell, worktiling.point_cells) +
-                         [(insert_cell[0] + 1, insert_cell[1] + 1)]),
-            positive_cells=translate_set(insert_cell,
-                                         worktiling.positive_cells),
-            possibly_empty=translate_set(insert_cell,
-                                         worktiling.possibly_empty),
-            obstructions=obs)
-    # for ob in worktiling.obstructions:
-        # print(ob)
-    # print(worktiling)
-
-    return worktiling
-
-
-def forced_obstructions_failed(placedidx, placedval, curpos,
-                               contr, patt, forces, pattpos):
-    assert len(curpos) == len(placedval)
-    assert len(placedidx) == len(placedval)
-    assert sorted([patt[i] for i in placedidx]) == placedval
-
-    pattinv = patt.inverse()
-
-    def insert_point(index, force=None):
-        idx = bisect_left(placedidx, index)
-        validx = bisect_left(placedval, patt[index])
-        if idx != len(placedidx) and placedidx[idx] == index:
-            return
-
-        mindex, maxdex, minval, maxval = 0, len(patt), 0, len(patt)
-        if idx > 0:
-            mindex = curpos[idx - 1][0]
-        if idx < len(curpos):
-            maxdex = curpos[idx][0]
-        if validx > 0:
-            loweridx = pattinv[placedval[validx - 1]]
-            minval = curpos[bisect_left(placedidx, loweridx)][1]
-        if validx < len(curpos):
-            # print(validx, placedval, curpos)
-            upperidx = pattinv[placedval[validx]]
-            maxval = curpos[bisect_left(placedidx, upperidx)][1]
-        if force == DIR_EAST:
-            mindex = pattpos[i][0] + 1
-        if force == DIR_NORTH:
-            minval = pattpos[i][1] + 1
-        if force == DIR_WEST:
-            maxdex = pattpos[i][0] - 1
-        if force == DIR_SOUTH:
-            maxval = pattpos[i][1] - 1
-
-        assert mindex <= maxdex
-        assert minval <= maxval
-
-        for x in range(mindex, maxdex + 1, 2):
-            for y in range(minval, maxval + 1, 2):
-                indices = placedidx[:]
-                indices.insert(idx, index)
-                vals = placedval[:]
-                vals.insert(idx, patt[index])
-                pos = curpos[:]
-                pos.insert(idx, (x, y))
-
-                perm = perm_from_points(patt, [(i * 2 + 1, patt[i] * 2 + 1)
-                                               for i in range(len(patt))],
-                                        Perm.to_standard(
-                                            patt[i] for i in indices),
-                                        [p for p in pos if p[0] % 2 == 0])
-
-                if perm.count_occurrences_of(patt) > 1:
-                    yield Obstruction(Perm(patt[i] for i in indices), pos)
-                else:
-                    for ob in forced_obstructions_failed(indices, vals, pos,
-                                                         contr, patt, forces,
-                                                         pattpos):
-                        yield ob
-    if contr:
-        for i in range(len(patt)):
-            for ob in insert_point(i):
-                print("CC")
-                yield ob
-    else:
-        for (i, f) in forces:
-            print(placedidx, placedval, curpos)
-            idx = bisect_left(placedidx, i)
-            if idx != len(placedidx) and placedidx[idx] == i:
-                continue
-            indices = placedidx[:]
-            indices.insert(idx, i)
-            vals = placedval[:]
-            vals.insert(idx, patt[i])
-            pos = curpos[:]
-            pos.insert(idx, pattpos[i])
-
-            for ob in forced_obstructions_failed(indices,
-                                                 vals,
-                                                 pos,
-                                                 False,
-                                                 patt,
-                                                 forces,
-                                                 pattpos):
-                print("AA")
-                yield ob
-            for ob in insert_point(idx, f):
-                print("BB")
-                yield ob
 
 
 def perm_from_points(patt1, pos1, patt2, pos2):
@@ -240,6 +67,8 @@ def perm_from_points(patt1, pos1, patt2, pos2):
                 in sorted(enumerate(sortedbyval),
                           key=lambda enumvalpoint: (enumvalpoint[1][1][0],
                                                     enumvalpoint[1][0][0])))
+
+    # Compute the indices of patt1 in the new permutation
     left, right = 0, 0
     fromleft = []
     for i in range(len(perm)):
@@ -266,10 +95,7 @@ def all_pattern_placements(patt, rows, columns):
         if idx == len(patt):
             yield pos
             return
-        # for i in range(len(patt)):
         validx = bisect_left(placedval, patt[idx])
-        print(patt, pos, idx)
-        print(validx)
         miny, maxy = 0, rows - 1
         placedval = placedval[:]
         if validx > 0:
@@ -285,28 +111,27 @@ def all_pattern_placements(patt, rows, columns):
         yield p
 
 
-def forced_obstructions(patt, force, pattpos):
+def forced_obstructions(patt, force, pattpos, legal_cells):
     subpatts = set()
     for length in range(1, len(patt) + 1):
         subpatts |= set(map(Perm.to_standard, combinations(patt, length)))
-    obs = list()
     force_strength = make_force_strength_func(force)
+    obs = list()
     for subpatt in sorted(subpatts):
         for pos in all_pattern_placements(subpatt,
-                                          2*len(patt) + 1,
+                                          2*len(patt) + 1,  # Assumes (0, 0)
                                           2*len(patt) + 1):
             (perm, pattidx) = perm_from_points(patt, pattpos, subpatt, pos)
             for occ in perm.occurrences_of(patt):
-                # print(perm, patt, occ, pattidx, force)
-                ob = Obstruction(subpatt, pos)
-                if (occ != pattidx and
-                        force_strength(occ, perm) > force_strength(pattidx, perm) and
-                        not any(o in ob for o in obs)):
-                    obs.append(ob)
+                if force_strength(occ, perm) > force_strength(pattidx, perm):
+                    ob = Obstruction(subpatt, pos)
+                    if (not any(o in ob for o in obs) and
+                            not any(p not in legal_cells for p in pos)):
+                        obs.append(ob)
     return obs
 
 
-def place_forced_pattern(tiling, patt, force, cell=(0, 0)):
+def place_pattern(tiling, patt):
     pattpos = []
     insertedval = list()
     for idx in range(len(patt)):
@@ -315,33 +140,50 @@ def place_forced_pattern(tiling, patt, force, cell=(0, 0)):
         insertedval.append(patt[idx])
         inserted_cell = (insert_cell[0] + 1, insert_cell[1] + 1)
         pattpos.append(inserted_cell)
-        tiling = Tiling(point_cells=translate_set(insert_cell, tiling.point_cells) + [inserted_cell],
-                        positive_cells=translate_set(insert_cell, tiling.positive_cells),
-                        possibly_empty=translate_set(insert_cell, tiling.possibly_empty),
-                        obstructions=chain.from_iterable(ob.place_point(insert_cell) for ob in tiling.obstructions),
+        tiling = Tiling(point_cells=(translate_set(insert_cell,
+                                                   tiling.point_cells) +
+                                     [inserted_cell]),
+                        positive_cells=translate_set(insert_cell,
+                                                     tiling.positive_cells),
+                        possibly_empty=translate_set(insert_cell,
+                                                     tiling.possibly_empty),
+                        obstructions=chain.from_iterable(
+                            ob.place_point(insert_cell, DIR_NONE)
+                            for ob in tiling.obstructions),
                         remove_empty=False)
+    return (tiling, pattpos)
 
-    # obstructions = tuple(forced_obstructions([], [], [], False, patt, force, pattpos))
-    obstructions = tuple(forced_obstructions(patt, force, pattpos))
-    print(obstructions)
-    for ob in obstructions:
-        print(ob)
-    return Tiling(
+
+def place_forced_pattern(tiling, patt, pattpos, force, cell=(0, 0)):
+    obstructions = tuple(forced_obstructions(patt, force, pattpos,
+                                             tiling.possibly_empty |
+                                             tiling.positive_cells))
+    tiling = Tiling(
         point_cells=tiling.point_cells,
         positive_cells=tiling.positive_cells,
         possibly_empty=tiling.possibly_empty,
-        obstructions=(tiling.obstructions +
-                      obstructions))
+        obstructions=tiling.obstructions + obstructions)
+    return tiling
 
 
 def forced_binary_pattern(tiling, **kwargs):
     if tiling.dimensions != (1, 1):
         return
 
-    maxlen = kwargs.get('maxlen')
+    maxpattlen = kwargs.get('pattlen')
+    maxforcelen = kwargs.get('forcelen')
     basis = [ob.patt for ob in tiling.obstructions]
+    if maxpattlen is None:
+        maxpattlen = max(map(len, basis)) - 1
+    if maxforcelen is None:
+        maxforcelen = maxpattlen
 
-    for pattlen in range(1, maxlen + 1):
+    for pattlen in range(1, maxpattlen + 1):
         for patt in PermSet(pattlen):
-            for force in binary_forces(patt, basis):
-                yield place_forced_pattern(tiling, patt, force)
+            (placedtiling, pattpos) = place_pattern(tiling, patt)
+            for force in binary_forces(patt, basis, maxforcelen):
+                # print("==============================================================")
+                til = place_forced_pattern(placedtiling, patt, pattpos, force)
+                # print(til)
+                yield til
+                # print(til)
