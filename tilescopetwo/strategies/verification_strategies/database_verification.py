973e9ee36f3172dfa824e643a6601ee5a00050cb
Tomas Ken <tomkenmag@gmail.com>
2017-10-12 17:18:58 +0000

    Added additional tilings to database

--- tilescopetwo/strategies/verification_strategies/database_verification.py
+++ tilescopetwo/strategies/verification_strategies/database_verification.py
@@ -4,10 +4,35 @@ from comb_spec_searcher import VerificationStrategy
 from grids_two import Tiling, Obstruction
 from permuta import Perm
 
-database = {Tiling(possibly_empty=[(0,0),(0,1)],
-                   obstructions=[Obstruction(Perm((0, 1)), [(0,0), (0,0)]),
-                                 Obstruction(Perm((0, 1, 2)), [(0,0), (0,1), (0,1)]),
-                                 Obstruction(Perm((0, 1, 2)), [(0,1),(0,1),(0,1)])])}
+database = {
+    Tiling(possibly_empty=[(0, 0), (0, 1)],
+           obstructions=[Obstruction(Perm((0, 1)),
+                                     [(0, 0), (0, 0)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(0, 0), (0, 1), (0, 1)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(0, 1), (0, 1), (0, 1)])]),
+    Tiling(possibly_empty=[(0, 0), (0, 1)],
+           obstructions=[Obstruction(Perm((0, 1)),
+                                     [(0, 1), (0, 1)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(0, 0), (0, 0), (0, 1)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(0, 0), (0, 0), (0, 0)])]),
+    Tiling(possibly_empty=[(0, 0), (1, 0)],
+           obstructions=[Obstruction(Perm((0, 1)),
+                                     [(1, 0), (1, 0)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(0, 0), (0, 0), (1, 0)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(0, 0), (0, 0), (0, 0)])]),
+    Tiling(possibly_empty=[(0, 0), (1, 0)],
+           obstructions=[Obstruction(Perm((0, 1)), [(0, 0), (0, 0)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(0, 0), (1, 0), (1, 0)]),
+                         Obstruction(Perm((0, 1, 2)),
+                                     [(1, 0), (1, 0), (1, 0)])])}
+
 
 def database_verified(tiling, **kwargs):
     if tiling in database:
