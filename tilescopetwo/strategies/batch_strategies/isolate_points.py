5c4d1086117d1d67ad54abed20d3830d1aa64942
Christian Bean <christianbean@ru.is>
2017-10-19 15:47:40 +0000

    removing some duplicates and debugging comments

--- tilescopetwo/strategies/batch_strategies/isolate_points.py
+++ tilescopetwo/strategies/batch_strategies/isolate_points.py
@@ -11,17 +11,6 @@ def point_isolations(tiling, **kwargs):
             # elif not equivalent and tiling.only_positive_in_col(cell):
             #     continue
             for strategy in isolate_point_in_column(tiling, cell):
-                # if tiling.dimensions == (4, 2):
-                #     print("=========================")
-                #     print("The tiling:")
-                #     print(tiling.to_old_tiling())
-                #     print(tiling)
-                #     print()
-                #     print("Gave the strategy:", strategy.formal_step)
-                #     for t in strategy.objects:
-                #         print(t.to_old_tiling())
-                #         print(t)
-                #         print()
                 yield strategy
 
         if not tiling.only_cell_in_row(cell):
@@ -30,17 +19,6 @@ def point_isolations(tiling, **kwargs):
             # elif not equivalent and tiling.only_positive_in_row(cell):
             #     continue
             for strategy in isolate_point_in_row(tiling, cell):
-                # if tiling.dimensions == (4, 2):
-                #     print("=========================")
-                #     print("The tiling:")
-                #     print(tiling.to_old_tiling())
-                #     print(tiling)
-                #     print()
-                #     print("Gave the strategy:", strategy.formal_step)
-                #     for t in strategy.objects:
-                #         print(t.to_old_tiling())
-                #         print(t)
-                #         print()
                 yield strategy
 
 def isolate_point_in_row(tiling, cell_to_be_isolated):
@@ -87,30 +65,10 @@ def isolate_point_in_row(tiling, cell_to_be_isolated):
                 y += 2
             possibly_empty.append((x, y))
 
-    # print(tiling.to_old_tiling())
-    # print(tiling)
-    # print(cell_to_be_isolated)
-    # print(positive_cells)
-    # print(point_cells)
-    # print(possibly_empty)
     obstructions = []
     for ob in tiling:
-        # print("========")
-        # L = [o for o in ob.isolate_point_row(cell_to_be_isolated)]
-        # print(ob)
-        # for o in L:
-        #     print(o)
         obstructions.extend([o for o in ob.isolate_point_row(cell_to_be_isolated)])
     if not in_row_points and not in_row_positive_cells:
-        # print("Isolated the point at {} in its row on the tiling".format(cell_to_be_isolated))
-        # print(tiling.to_old_tiling())
-        # print("it gave")
-        # print(Tiling(positive_cells=positive_cells,
-        #         point_cells=point_cells,
-        #         possibly_empty=possibly_empty,
-        #         obstructions=obstructions).to_old_tiling())
-        # print()
-        # print("---------")
         yield BatchStrategy("Isolated the point at {} in its row".format(cell_to_be_isolated),
                             [Tiling(positive_cells=positive_cells,
                                     point_cells=point_cells,
@@ -164,18 +122,13 @@ def isolate_point_in_row(tiling, cell_to_be_isolated):
                 y+= 2
                 below_point_cells.append((x, y))
                 above_point_cells.append((x, y))
-        # print(tiling)
-        # print(above_point_cells, above_empty_cells, above_possibly_empty)
         above_obstructions = [o for o in obstructions if not any(o.occupies(c) for c in above_empty_cells)]
-        # for o in above_obstructions:
-        #      print(o)
         above_tiling = Tiling(point_cells=above_point_cells,
                               possibly_empty=above_possibly_empty,
                               positive_cells=above_positive_cells,
                               obstructions=[o for o in obstructions
                                             if not any(o.occupies(c)
                                                        for c in above_empty_cells)])
-        # print(above_tiling)
         below_tiling = Tiling(point_cells=below_point_cells,
                               possibly_empty=below_possibly_empty,
                               positive_cells=below_positive_cells,
@@ -186,16 +139,9 @@ def isolate_point_in_row(tiling, cell_to_be_isolated):
         isolated_tilings.append((above_tiling, below_tiling))
 
     for i in range(2):
-
-        # print("Isolated the point at {} in its row on the tiling".format(cell_to_be_isolated))
-        # print(tiling.to_old_tiling())
-        # print("it gave")
-        # for t in [t[i] for t in isolated_tilings]:
-        #     print(t.to_old_tiling())
-        #     print()
-        # print()
-        # print("---------")
         yield BatchStrategy("Isolated the point at {} in its row".format(cell_to_be_isolated), [t[i] for t in isolated_tilings])
+        if not in_row_positive_cells:
+            break
 
 
 def isolate_point_in_column(tiling, cell_to_be_isolated):
@@ -241,31 +187,10 @@ def isolate_point_in_column(tiling, cell_to_be_isolated):
             if x > cell_to_be_isolated[0]:
                 x += 2
             possibly_empty.append((x, y))
-
-    # print(tiling.to_old_tiling())
-    # print(tiling)
-    # print(cell_to_be_isolated)
-    # print(positive_cells)
-    # print(point_cells)
-    # print(possibly_empty)
     obstructions = []
     for ob in tiling:
-        # print("========")
-        # L = [o for o in ob.isolate_point_row(cell_to_be_isolated)]
-        # print(ob)
-        # for o in L:
-        #     print(o)
         obstructions.extend([o for o in ob.isolate_point_col(cell_to_be_isolated)])
     if not in_col_points and not in_col_positive_cells:
-        # print("Isolated the point at {} in its col on the tiling".format(cell_to_be_isolated))
-        # print(tiling.to_old_tiling())
-        # print("it gave")
-        # print(Tiling(positive_cells=positive_cells,
-        #         point_cells=point_cells,
-        #         possibly_empty=possibly_empty,
-        #         obstructions=obstructions).to_old_tiling())
-        # print()
-        # print("---------")
         yield BatchStrategy("Isolated the point at {} in its col".format(cell_to_be_isolated),
                             [Tiling(positive_cells=positive_cells,
                                     point_cells=point_cells,
@@ -319,18 +244,13 @@ def isolate_point_in_column(tiling, cell_to_be_isolated):
                 x += 2
                 below_point_cells.append((x, y))
                 above_point_cells.append((x, y))
-        # print(tiling)
-        # print(above_point_cells, above_empty_cells, above_possibly_empty)
         above_obstructions = [o for o in obstructions if not any(o.occupies(c) for c in above_empty_cells)]
-        # for o in above_obstructions:
-        #      print(o)
         above_tiling = Tiling(point_cells=above_point_cells,
                               possibly_empty=above_possibly_empty,
                               positive_cells=above_positive_cells,
                               obstructions=[o for o in obstructions
                                             if not any(o.occupies(c)
                                                        for c in above_empty_cells)])
-        # print(above_tiling)
         below_tiling = Tiling(point_cells=below_point_cells,
                               possibly_empty=below_possibly_empty,
                               positive_cells=below_positive_cells,
@@ -341,16 +261,9 @@ def isolate_point_in_column(tiling, cell_to_be_isolated):
         isolated_tilings.append((above_tiling, below_tiling))
 
     for i in range(2):
-
-        # print("Isolated the point at {} in its col on the tiling".format(cell_to_be_isolated))
-        # print(tiling.to_old_tiling())
-        # print("it gave")
-        # for t in [t[i] for t in isolated_tilings]:
-        #     print(t.to_old_tiling())
-        #     print()
-        # print()
-        # print("---------")
         yield BatchStrategy("Isolated the point at {} in its col".format(cell_to_be_isolated), [t[i] for t in isolated_tilings])
+        if not in_col_positive_cells:
+            break
 
 
 def powerset(l):
