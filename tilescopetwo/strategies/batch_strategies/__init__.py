78a6fa69192f7122a83c1fd4c5ad895e6c9e0db5
Tomas Ken <tomkenmag@gmail.com>
2017-10-20 16:17:42 +0000

    Added the binary forced pattern placement with requirements, now as an equivalence strategy

--- tilescopetwo/strategies/batch_strategies/__init__.py
+++ tilescopetwo/strategies/batch_strategies/__init__.py
@@ -1,10 +1,7 @@
 from comb_spec_searcher import BatchStrategy
 
-from .cell_insertion import all_cell_insertions
-
-from .row_and_column_placements import row_placements
-from .row_and_column_placements import col_placements
-
-from .isolate_points import point_isolations
-
 from .binary_pattern import forced_binary_pattern
+from .cell_insertion import all_cell_insertions
+from .isolate_points import point_isolations
+from .requirement_insertion import all_requirement_insertions
+from .row_and_column_placements import col_placements, row_placements
