1f887b778560310e923f1f86cb0a9acc1d6bb8f4
RagnarArdal <RagnarArdal@gmail.com>
2017-10-26 09:57:27 +0000

    The new visual queue just seems to work. Now just to add the feature of hovering highlighting.

--- comb_spec_searcher/visual_queue.py
+++ comb_spec_searcher/visual_queue.py
@@ -1,4 +1,4 @@
-from threading import Lock  # I don't know if mpl uses threading, but lets hope for the best
+from threading import RLock  # I don't know if mpl uses threading, but lets hope for the best
 from collections import deque, defaultdict
 
 import enum
@@ -102,7 +102,7 @@ class ZoomPan:
         return onMotion
 
 
-DEBUG = False
+DEBUG = True
 
 
 def debug_print(*args, **kwargs):
@@ -112,7 +112,7 @@ def debug_print(*args, **kwargs):
 
 NODE_RADIUS = .2
 LEVEL_DIFF = 1
-LINE_WIDTH = 1
+LINE_WIDTH = 3
 
 
 class Color:
@@ -188,6 +188,7 @@ class RawTree(AbstractOrderedTree):
             children=(),
             data=None,
             description_getter=lambda raw_tree: "NO DESCRIPTION",
+            identifier=None,
         ):
         self.label = label
 
@@ -199,6 +200,8 @@ class RawTree(AbstractOrderedTree):
 
         self.patch = None
 
+        self.identifier = identifier
+
 
 # The Queue class itself, used in the tilescope to present the next label needing work
 
@@ -208,7 +211,10 @@ class VisualQueue:
         self.tilescope = tilescope
         self.queue = deque()
         self.last_hover = None
-        self.lock = Lock()
+        self.lock = RLock()
+        self.expanded = set()  # List of obj node labels that have been passed to tree
+        self.show_children = set()  # List of node identifiers that has been clicked on (alternates to off)
+        self.nodes = set()
         # Create figure
         self.fig = plt.figure()
         # Register event handlers
@@ -238,9 +244,7 @@ class VisualQueue:
 
     def hover_event_handler(self, event):
         with self.lock:
-            for patch in self.ax.patches:
-                if not isinstance(patch, VisNode):
-                    continue
+            for patch in self.nodes:
                 if patch.contains(event)[0] and patch != self.last_hover:
                     self.last_hover = patch
                     print()
@@ -251,14 +255,47 @@ class VisualQueue:
                     break
 
     def pick_event_handler(self, event):
+        if event.mouseevent.button != 1:
+            # We only care about clicking on the nodes
+            return
         with self.lock:
-            self.queue.append(event.artist.raw_tree.label)
+            if isinstance(event.artist, VisNode):
+                if isinstance(event.artist, CtrlNode):
+                    identifier = event.artist.raw_tree.identifier
+                    if identifier in self.show_children:
+                        debug_print("+++ Remove id from show children ids:", identifier)
+                        self.show_children.remove(identifier)
+                    else:
+                        debug_print("+++ Add id to show children ids:", identifier)
+                        self.show_children.add(identifier)
+                    self.redraw_tree()
+                elif isinstance(event.artist, ObjNode):
+                    if Flag.VERIFIED in event.artist.raw_tree.data:
+                        debug_print("+++ Ignore click because node is verified obj node")
+                        return
+                    label = event.artist.raw_tree.label
+                    if label in self.show_children:
+                        debug_print("+++ Remove label from show children labels:", label)
+                        self.show_children.remove(label)
+                        # Has already been expanded
+                        self.redraw_tree()
+                    else:
+                        debug_print("+++ Add label to show children labels:", label)
+                        self.show_children.add(label)
+                        if label in self.expanded:
+                            self.redraw_tree()
+                        else:
+                            self.expanded.add(label)
+                            self.queue.append(event.artist.raw_tree.label)
+            else:
+                raise RuntimeError("Unexpected non-node picker:", event.artist)
 
     def redraw_tree(self):
         debug_print("+++ Waiting on lock to redraw")
         with self.lock:
             debug_print("+++ Acquired lock to redraw")
 
+            self.nodes = set()
             self.ax.cla()  # Clear axes
 
             # Construct draw-able tree from tilescope data
@@ -299,29 +336,36 @@ class VisualQueue:
                     debug_print("+++ It has been seen before")
                     raw_tree.data.add(Flag.PHANTOM)
                     # We don't draw any of its children, so we're done
+                elif raw_tree.label not in self.show_children:
+                    debug_print("+++ User doesn't want its child ctrl nodes displayed")
                 else:
                     # Add branching things
                     for branch_labels in self.tilescope.ruledb.rules_dict[raw_tree.label]:
                         debug_print("+++ It branches to", branch_labels)
-                        branch_control_node = RawTree(children=[])
-                        branch_control_node.data.add(Flag.CONTROL)
-                        branch_control_node.data.add(
+                        branch_ctrl_identifier = (raw_tree.label, branch_labels)
+                        branch_ctrl_raw_tree = RawTree(children=[], identifier=branch_ctrl_identifier)
+                        branch_ctrl_raw_tree.data.add(Flag.CONTROL)
+                        branch_ctrl_raw_tree.data.add(
                             Flag.PRODUCT
                             if raw_tree.label in self.tilescope.ruledb.back_maps
                             and branch_labels in self.tilescope.ruledb.back_maps[raw_tree.label]
                             else
                             Flag.SUM
                         )
-                        branch_control_node.get_description = lambda l=raw_tree.label, \
+                        branch_ctrl_raw_tree.get_description = lambda l=raw_tree.label, \
                                                                      ls=branch_labels: \
                             self.tilescope.ruledb.explanation(l, ls)
-                        for branch_label in branch_labels:
-                            branch_node = RawTree(branch_label)
-                            branch_control_node.children.append(branch_node)
-                            frontier.append(branch_node)
-                        raw_tree.children.append(branch_control_node)
+                        if branch_ctrl_identifier in self.show_children:
+                            # Add children
+                            for branch_label in branch_labels:
+                                branch_obj_raw_tree = RawTree(branch_label)
+                                branch_ctrl_raw_tree.children.append(branch_obj_raw_tree)
+                                frontier.append(branch_obj_raw_tree)
+                        raw_tree.children.append(branch_ctrl_raw_tree)
+
                     # Add equivalent things to tree and frontier
                     if any(eqv_label in seen for eqv_label in self.tilescope.equivdb.equivalent_set(raw_tree.label)):
+                        # Don't draw any equivalent branching if it has been done for some equivalent node
                         continue
                     # Mark as seen, all later-found nodes for same label will be phantoms
                     seen.add(raw_tree.label)
@@ -330,15 +374,17 @@ class VisualQueue:
                             debug_print("+++ Ignoring itself in eqv draw")
                             continue
                         debug_print("+++ It has an eqv node label of", eqv_label)
-                        eqv_ctrl_raw_tree = RawTree()
+                        eqv_ctrl_identifier = (raw_tree.label, 0, eqv_label)
+                        eqv_ctrl_raw_tree = RawTree(identifier=eqv_ctrl_identifier)
                         eqv_ctrl_raw_tree.data.add(Flag.CONTROL)
                         eqv_ctrl_raw_tree.data.add(Flag.EQUIVALENCE)
                         eqv_ctrl_raw_tree.get_description = lambda l=raw_tree.label, el=eqv_label: \
                             self.tilescope.equivdb.get_explanation(l, el)
-                        eqv_obj_raw_tree = RawTree(eqv_label)
-                        eqv_obj_raw_tree.data.add(Flag.EQUIVALENCE)
-                        frontier.append(eqv_obj_raw_tree)
-                        eqv_ctrl_raw_tree.children = (eqv_obj_raw_tree,)
+                        if eqv_ctrl_identifier in self.show_children:
+                            eqv_obj_raw_tree = RawTree(eqv_label)
+                            eqv_obj_raw_tree.data.add(Flag.EQUIVALENCE)
+                            frontier.append(eqv_obj_raw_tree)
+                            eqv_ctrl_raw_tree.children = (eqv_obj_raw_tree,)
                         raw_tree.children.append(eqv_ctrl_raw_tree)
 
             debug_print("+++ AFTER We have a rules dict:")
@@ -381,8 +427,6 @@ class VisualQueue:
                     # Node is an object node
                     node = ObjNode(xy, raw_tree)
                     if Flag.EXPANDABLE in data:
-                        # Node is a picker, can be clicked to expand
-                        node.set_picker(True)
                         # Change color to notify of expandability
                         node.set_color(Color.PICKABLE)
                     elif Flag.VERIFIED in data:
@@ -402,6 +446,7 @@ class VisualQueue:
                         text_content = "+"
                     else:
                         raise RuntimeError("Unknown control node type for drawing")
+                    # Make text on top of it
                     text_path = TextPath(
                         (x - (.09 if text_content == "x" else .125), y-.09),
                         text_content,
@@ -415,7 +460,9 @@ class VisualQueue:
 
                 debug_print()
 
+                node.set_picker(True)
                 self.ax.add_patch(node)
+                self.nodes.add(node)
 
                 # Add children to drawing frontier as well
                 frontier.extend((child_raw, child_draw, xy)
