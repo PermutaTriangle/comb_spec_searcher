27d2245005e0cc74466078ab402a54750986d807
Christian Bean <christianbean@ru.is>
2017-11-02 17:40:14 +0000

    counting subob inferral is broken - remove from packs

--- tilescopetwo/strategy_packs.py
+++ tilescopetwo/strategy_packs.py
@@ -7,19 +7,19 @@ from permuta import Perm
 # 'subobstruction_inferral_rec' but it is a lot slower. The function
 # 'subobstruction_inferral' is a subset of the work.
 
-point_placement_with_subobstruction_inferral = ATRAPStrategyPack(
+point_placement = ATRAPStrategyPack(
          eq_strats=[all_point_placements],
          ver_strats=[subset_verified],
-         inf_strats=[subobstruction_inferral, row_and_column_separation],
+         inf_strats=[row_and_column_separation],
          other_strats=[[components], [all_cell_insertions]],
-         name="point_placement_with_subobstruction_inferral")
+         name="point_placement")
 
 point_placement_one_cell_inferral = ATRAPStrategyPack(
          eq_strats=[all_point_placements],
          ver_strats=[subset_verified],
-         inf_strats=[subobstruction_inferral],
+         inf_strats=[],
          other_strats=[[components], [all_cell_insertions]],
-         name="point_placement_one_cell_inferral")
+         name="point_placement")
 
 point_placement_no_infer = ATRAPStrategyPack(
          eq_strats=[all_point_placements],
@@ -28,50 +28,50 @@ point_placement_no_infer = ATRAPStrategyPack(
          other_strats=[[components], [all_cell_insertions]],
          name="point_placement_no_infer")
 
-row_placements_with_subobstruction_inferral = ATRAPStrategyPack(
+row_placements_only = ATRAPStrategyPack(
          eq_strats=[],
          ver_strats=[subset_verified],
-         inf_strats=[subobstruction_inferral, row_and_column_separation],
+         inf_strats=[row_and_column_separation],
          other_strats=[[components],[all_cell_insertions, row_placements]],
-         name="row_placements_with_subobstruction_inferral")
+         name="row_placements")
 
-col_placements_with_subobstruction_inferral = ATRAPStrategyPack(
+col_placements_only = ATRAPStrategyPack(
          eq_strats=[],
          ver_strats=[subset_verified],
-         inf_strats=[subobstruction_inferral, row_and_column_separation],
+         inf_strats=[row_and_column_separation],
          other_strats=[[components], [all_cell_insertions, col_placements]],
-         name="col_placements_with_subobstruction_inferral")
+         name="col_placements")
 
-row_and_column_placements_with_subobstruction_inferral = ATRAPStrategyPack(
+row_and_column_placements = ATRAPStrategyPack(
          eq_strats=[],
          ver_strats=[subset_verified],
-         inf_strats=[subobstruction_inferral, row_and_column_separation],
+         inf_strats=[row_and_column_separation],
          other_strats=[[components],
                        [all_cell_insertions, row_placements, col_placements]],
-         name="row_and_column_placements_with_subobstruction_inferral")
+         name="row_and_column_placements")
 
-row_and_column_placements_with_subobstruction_inferral_and_database = ATRAPStrategyPack(
+row_and_column_placements_and_database = ATRAPStrategyPack(
          eq_strats=[],
          ver_strats=[subset_verified, database_verified],
-         inf_strats=[subobstruction_inferral, row_and_column_separation],
+         inf_strats=[row_and_column_separation],
          other_strats=[[components],
                        [all_cell_insertions, row_placements, col_placements]],
-         name="row_and_column_placements_with_subobstruction_inferral_and_database")
+         name="row_and_column_placements_and_database")
 
 point_separation_and_row_col_placements = ATRAPStrategyPack(
          eq_strats=[point_separation],
          ver_strats=[subset_verified],
-         inf_strats=[subobstruction_inferral, row_and_column_separation],
+         inf_strats=[row_and_column_separation],
          other_strats=[[components],
                        [all_cell_insertions,
                         partial(row_placements, all_positive_in_row=False),
                         partial(col_placements, all_positive_in_col=False)]],
-         name="row_and_column_placements_with_subobstruction_inferral")
+         name="row_and_column_placements")
 
 binary_force_only = ATRAPStrategyPack(
     eq_strats=[forced_binary_pattern],
     ver_strats=[subset_verified],
-    inf_strats=[subobstruction_inferral, row_and_column_separation],
+    inf_strats=[row_and_column_separation],
     other_strats=[[partial(components, unions=True)],
                   [all_cell_insertions],
                   [partial(all_requirement_insertions, maxreqlength=4)],
@@ -81,7 +81,7 @@ binary_force_only = ATRAPStrategyPack(
 point_separation_and_isolation = ATRAPStrategyPack(
          eq_strats=[point_separation],
          ver_strats=[subset_verified],
-         inf_strats=[subobstruction_inferral, row_and_column_separation],
+         inf_strats=[row_and_column_separation],
          other_strats=[[components],
                        [all_cell_insertions, point_isolations]],
          name="point_separation_and_isolation")
