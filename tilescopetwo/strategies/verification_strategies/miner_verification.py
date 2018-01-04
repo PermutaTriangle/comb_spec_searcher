2a5cbaac72a53551dfac2b20bf508301d5c93398
Christian Bean <christianbean@ru.is>
2018-01-04 11:50:53 +0000

    all other variations

--- tilescopetwo/strategies/verification_strategies/miner_verification.py
+++ tilescopetwo/strategies/verification_strategies/miner_verification.py
@@ -7,8 +7,8 @@ def miner_verified(tiling, basis, **kwargs):
         (tiling.dimensions[0] is 2 and tiling.dimensions[1] is 1)):
         top = []
         bottom = []
-        topbool = False
-        bottombool = False
+        topbool = None
+        bottombool = None
         for ob in tiling:
             if ob.is_single_cell():
                 if ob.pos[0] == (0, 0):
@@ -16,23 +16,56 @@ def miner_verified(tiling, basis, **kwargs):
                 else:
                     top.append(ob.patt)
             else:
-                if topbool or bottombool:
+                if topbool is not None or bottombool is not None:
                     return None
                 if ob.patt == Perm((0, 1, 2)):
                     if ob.pos[0] == (0, 0) and ob.pos[1] == (0, 0):
-                        bottombool = True
+                        bottombool = 0
                     else:
-                        topbool = True
+                        topbool = 0
                 elif ob.patt == Perm((2, 1, 0)):
                     if ob.pos[1] == (0, 0) and ob.pos[2] == (0, 0):
-                        bottombool = True
+                        bottombool = 1
                     elif ob.pos[0] == (0, 1) and ob.pos[1] == (0, 1):
-                        topbool = True
-        if topbool:
-            Basis(top) == Basis([Perm((0, 1, 2))])
-            if Basis(bottom) != basis:
-                return VerificationStrategy("Miner verified!")
-        elif bottombool:
-            Basis(bottom) == Basis([Perm((0, 1, 2))])
-            if Basis(top) != basis:
-                return VerificationStrategy("Miner verified!")
+                        topbool = 1
+                    else:
+                        return None
+                elif ob.patt == Perm((0, 2, 1)):
+                    if ob.pos[1] == ob.pos[2]:
+                        topbool = 1
+                    else:
+                        return None
+                elif ob.patt == Perm((1, 0, 2)):
+                    if ob.pos[0] == ob.pos[1]:
+                        bottombool = 1
+                    else:
+                        return None
+                elif ob.patt == Perm((1, 2, 0)):
+                    if ob.pos[0] == (0, 0) and ob.pos[1] == (0, 0):
+                        bottombool = 0
+                    elif ob.pos[0] == (0, 1) and ob.pos[1] == (0, 1):
+                        topbool = 0
+                    else:
+                        return None
+                elif ob.patt == Perm((2, 0, 1)):
+                    if ob.pos[1] == (0, 0) and ob.pos[2] == (0, 0):
+                        bottombool = 0
+                    elif ob.pos[1] == (1, 0) and ob.pos[2] == (0, 0):
+                        topbool = 0
+                    else:
+                        return None
+
+                else:
+                    return None
+        if topbool is not None:
+            topbasis = Basis(top)
+            if ((topbool == 0 and topbasis == Basis([Perm((0, 1, 2))]))
+                or (topbool == 1 and topbasis == Basis([Perm((2, 1, 0))]))):
+                if Basis(bottom) != basis:
+                    return VerificationStrategy("Miner verified!")
+        elif bottombool is not None:
+            bottombasis = Basis(bottom)
+            if ((bottombool == 0 and Basis(bottom) == Basis([Perm((0, 1, 2))]))
+                or (bottombool == 1 and bottombasis == Basis([Perm((2, 1, 0))]))):
+                if Basis(top) != basis:
+                    return VerificationStrategy("Miner verified!")
