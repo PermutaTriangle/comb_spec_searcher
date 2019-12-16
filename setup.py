701585339b5e0d765aec72e42d5739d01b8d4b34
Émile Nadeau <nadeau.emile@gmail.com>
2019-12-16 11:16:51 +0000

    [requires.io] dependency update (#53)

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     ],
     setup_requires=['pytest-runner==5.2'],
     tests_require=[
-        'pytest==5.3.1',
+        'pytest==5.3.2',
         'pytest-cov==2.8.1',
         'pytest-isort==0.3.1',
         'pytest-pep8==1.0.6',
