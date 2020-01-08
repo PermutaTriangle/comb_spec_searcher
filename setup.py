ef3a0f5970ba3f84f7fcf451cf5b2042a5a5d8de
Émile Nadeau <nadeau.emile@gmail.com>
2020-01-08 21:15:09 +0000

    [requires.io] dependency update (#55)
    Co-authored-by: requires.io <support@requires.io>

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     ],
     install_requires=[
         'logzero==1.5.0',
-        'sympy==1.5',
+        'sympy==1.5.1',
         'psutil==5.6.7'
     ],
     setup_requires=['pytest-runner==5.2'],
