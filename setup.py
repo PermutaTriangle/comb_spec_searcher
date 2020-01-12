5c83d7a14acc1e6323399b631e55490739a8d264
Émile Nadeau <nadeau.emile@gmail.com>
2020-01-12 15:43:55 +0000

    [requires.io] dependency update (#56)
    Co-authored-by: requires.io <support@requires.io>

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         'pytest-isort==0.3.1',
         'pytest-pep8==1.0.6',
         'pytest-repeat==0.8.0',
-        'docutils==0.15.2',
+        'docutils==0.16',
         'Pygments==2.5.2'
     ]
 )
