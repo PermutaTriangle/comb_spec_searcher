5dccec454f5fe2ac1da58cf6d86e2cdada534298
Émile Nadeau <nadeau.emile@gmail.com>
2020-05-18 13:14:20 -0400

    Restructure cssqueue (#90)
    * Update class_queue.py
    
    * turn work packet into namedtuple
    
    * delay to ignore check to `__next__`
    
    * remove the iterator attribute of the class queue
    
    * reformat next and populate methods
    
    * fix the queue size update
    
    * implement __eq__ for class queue
    
    * test pickle on classdb
    
    * keeping the sets as small as possible
    
    * cleaning the __eq__
    
    * remove redundant method
    
    * fix a bug in can_do_expansion
    
    The can_do_expansion function of the class queue did not check if the
    class was ignored hence creating an infinite loop in the queue. This is
    now fixed
    
    Co-authored-by: Christian Bean <christianbean@ru.is>

--- comb_spec_searcher/class_queue.py
+++ comb_spec_searcher/class_queue.py
@@ -4,12 +4,16 @@ A queue of labels.
 import abc
 from collections import Counter, deque
 from typing import Counter as CounterType
-from typing import Deque, Iterator, List, Set, Tuple
+from typing import Deque, Iterator, List, NamedTuple, Set, Tuple
 
 from .strategies.strategy import CSSstrategy
 from .strategies.strategy_pack import StrategyPack
 
-WorkPacket = Tuple[int, Tuple[CSSstrategy, ...], bool]
+
+class WorkPacket(NamedTuple):
+    label: int
+    strategies: Tuple[CSSstrategy, ...]
+    inferral: bool
 
 
 class CSSQueue(abc.ABC):
@@ -21,7 +25,11 @@ class CSSQueue(abc.ABC):
         self.inferral_strategies = tuple(pack.inferral_strats)
         self.initial_strategies = tuple(pack.initial_strats)
         self.expansion_strats = tuple(tuple(x) for x in pack.expansion_strats)
-        self.iterator = self._iter_helper()
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, CSSQueue):
+            return NotImplemented
+        return self.__class__ == other.__class__ and self.__dict__ == other.__dict__
 
     @abc.abstractmethod
     def add(self, label: int) -> None:
@@ -50,8 +58,11 @@ class CSSQueue(abc.ABC):
     def status(self) -> str:
         """Return a string that indicates that current status of the queue."""
 
+    def __iter__(self) -> Iterator[WorkPacket]:
+        return self
+
     @abc.abstractmethod
-    def _iter_helper(self) -> Iterator[WorkPacket]:
+    def __next__(self) -> WorkPacket:
         """
         Yield the combinatorial classes in queue.
         It should yield triples (label, strategies, inferral)
@@ -62,19 +73,7 @@ class CSSQueue(abc.ABC):
         children, and assume you are ignore the parent from now on. The
         strategies will be applied cyclically until no change.
         """
-
-    def __iter__(self) -> Iterator[WorkPacket]:
-        while True:
-            try:
-                yield next(self)
-            except StopIteration:
-                break
-
-    def __next__(self) -> WorkPacket:
-        try:
-            return next(self.iterator)
-        except StopIteration:
-            raise StopIteration("No more classes to expand.")
+        raise NotImplementedError
 
 
 class DefaultQueue(CSSQueue):
@@ -87,14 +86,19 @@ class DefaultQueue(CSSQueue):
         self.working: Deque[int] = deque()
         self.next_level: CounterType[int] = Counter()
         self.curr_level: Deque[int] = deque()
-        self.inferral_expanded: Set[int] = set()
-        self.initial_expanded: Set[int] = set()
-        self.expansion_expanded: List[Set[int]] = [
+        self._inferral_expanded: Set[int] = set()
+        self._initial_expanded: Set[int] = set()
+        self._expansion_expanded: List[Set[int]] = [
             set() for _ in range(len(self.expansion_strats))
         ]
         self.ignore: Set[int] = set()
-        self.inferral_ignore: Set[int] = set()
         self.queue_sizes: List[int] = []
+        self.staging: Deque[WorkPacket] = deque([])
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, DefaultQueue):
+            return NotImplemented
+        return self.__class__ == other.__class__ and self.__dict__ == other.__dict__
 
     @property
     def levels_completed(self):
@@ -111,99 +115,103 @@ class DefaultQueue(CSSQueue):
         self.set_stop_yielding(label)
 
     def set_not_inferrable(self, label: int) -> None:
+        """Mark the label such that it's not expanded with inferral anymore"""
         if label not in self.ignore:
-            self.inferral_expanded.add(label)
+            self._inferral_expanded.add(label)
+
+    def set_not_initial(self, label: int) -> None:
+        """Mark the label such that it's not expanded with initial anymore"""
+        if label not in self.ignore:
+            self._initial_expanded.add(label)
+
+    def set_expansion_expanded(self, label: int, idx: int) -> None:
+        """
+        Mark the label such that it's not expanded with expansion for the given
+        indices anymore
+        """
+        if label not in self.ignore:
+            self._expansion_expanded[idx].add(label)
 
     def set_stop_yielding(self, label: int) -> None:
-        self._add_to_ignore(label)
-
-    def _add_to_ignore(self, label: int) -> None:
         self.ignore.add(label)
         # can remove it elsewhere to keep sets "small"
-        self.inferral_ignore.discard(label)
-        self.inferral_expanded.discard(label)
-        self.initial_expanded.discard(label)
-        for s in self.expansion_expanded:
+        self._inferral_expanded.discard(label)
+        self._initial_expanded.discard(label)
+        for s in self._expansion_expanded:
             s.discard(label)
         self.next_level.pop(label, None)
 
     def can_do_inferral(self, label: int) -> bool:
         """Return true if inferral strategies can be applied."""
-        return bool(
-            self.inferral_strategies
-            and label not in self.ignore
-            and label not in self.inferral_ignore
-        )
+        return bool(self.inferral_strategies) and label not in self._inferral_expanded
 
     def can_do_initial(self, label: int) -> bool:
         """Return true if initial strategies can be applied."""
-        return bool(
-            self.initial_strategies
-            and label not in self.ignore
-            and label not in self.initial_expanded
-        )
+        return bool(self.initial_strategies) and label not in self._initial_expanded
 
     def can_do_expansion(self, label: int, idx: int) -> bool:
         """Return true if expansion strategies can be applied."""
-        return label not in self.ignore and label not in self.expansion_expanded[idx]
+        return (
+            label not in self.ignore
+            and idx < len(self.expansion_strats)
+            and label not in self._expansion_expanded[idx]
+        )
 
-    def _iter_helper(self) -> Iterator[WorkPacket]:
+    def _populate_staging(self) -> None:
         """
-        Yield the next combinatorial class in current queue.
+        Populate the staging queue that is used by next to return WorkPacket.
+        """
+        if not self.staging and self.working:
+            self.staging.extend(self._iter_helper_working())
+        if not self.staging:
+            if not self.curr_level:
+                self._change_level()
+            if self.curr_level:
+                self.staging.extend(self._iter_helper_curr())
+        if not self.staging:
+            raise StopIteration("No more classes to expand")
 
-        If current queue becomes empty will change next queue to current and
-        return first combinatorial class. Return None if no combinatorial
-        classes to expand.
-        """
-        # pylint: disable=too-many-nested-blocks
+    def _change_level(self) -> None:
+        self.curr_level = deque(
+            label
+            for label, _ in sorted(self.next_level.items(), key=lambda x: -x[1])
+            if self.expansion_strats and self.can_do_expansion(label, 0)
+        )
         self.queue_sizes.append(len(self.curr_level))
-        while True:
-            if self.working:
-                label = self.working.popleft()
-                if self.can_do_inferral(label):
-                    yield label, self.inferral_strategies, True
-                    self.inferral_expanded.add(label)
-                for strat in self.initial_strategies:
-                    if self.can_do_initial(label):
-                        yield label, (strat,), False
-                    else:
-                        break
-                else:
-                    if label not in self.ignore:
-                        self.initial_expanded.add(label)
-                self.next_level.update((label,))
+        self.next_level = Counter()
 
-            elif self.curr_level:
-                label = self.curr_level.popleft()
-                for idx, strats in enumerate(self.expansion_strats):
-                    if self.can_do_expansion(label, idx):
-                        for strat in strats:
-                            if self.can_do_expansion(label, idx):
-                                yield label, (strat,), False
-                            else:
-                                break
-                        else:
-                            self.expansion_expanded[idx].add(label)
-                        if idx + 1 < len(self.expansion_strats) and (
-                            self.can_do_expansion(label, idx + 1)
-                        ):
-                            self.curr_level.append(label)
-                        break
-                else:
-                    # finished applying all strategies to this label, ignore from now on
-                    self._add_to_ignore(label)
-            else:
-                # input("Finished level {}".format(self.levels_completed))
-                if not self.next_level:
-                    return None
-                self.curr_level = deque(
-                    label
-                    for label, _ in sorted(self.next_level.items(), key=lambda x: -x[1])
-                    if self.expansion_strats and self.can_do_expansion(label, 0)
-                )
-                self.queue_sizes.append(len(self.curr_level))
-                self.next_level = Counter()
-                continue
+    def _iter_helper_curr(self) -> Iterator[WorkPacket]:
+        label = self.curr_level.popleft()
+        for idx, strats in enumerate(self.expansion_strats):
+            if self.can_do_expansion(label, idx):
+                for strat in strats:
+                    yield WorkPacket(label, (strat,), False)
+                self.set_expansion_expanded(label, idx)
+                if self.can_do_expansion(label, idx + 1):
+                    self.curr_level.append(label)
+                break
+        else:
+            # finished applying all strategies to this label, ignore from now on
+            self.set_stop_yielding(label)
+
+    def _iter_helper_working(self) -> Iterator[WorkPacket]:
+        label = self.working.popleft()
+        if self.can_do_inferral(label):
+            yield WorkPacket(label, self.inferral_strategies, True)
+            self.set_not_inferrable(label)
+        if self.can_do_initial(label):
+            for strat in self.initial_strategies:
+                yield WorkPacket(label, (strat,), False)
+            self.set_not_initial(label)
+        self.next_level.update((label,))
+
+    def __next__(self) -> WorkPacket:
+        while self.staging:
+            wp = self.staging.popleft()
+            if wp.label not in self.ignore:
+                return wp
+        self._populate_staging()
+        return next(self)
 
     def do_level(self) -> Iterator[WorkPacket]:
         """
