dc906ef16ee3bf5afe772220559cc38d590c2e64
Christian Bean <christianbean@ru.is>
2020-06-25 19:30:10 +0200

    double check rule is the rule we want (#138)
    * double check rule is the rule we want
    
    * see the previous commit, just doing it properly now!

--- comb_spec_searcher/rule_db/forget.py
+++ comb_spec_searcher/rule_db/forget.py
@@ -59,13 +59,14 @@ class RecomputingDict(MutableMapping[RuleKey, AbstractStrategy]):
             )
             for rule in rules:
                 try:
+                    start_label = self.classdb.get_label(rule.comb_class)
                     nonempty_children = tuple(
                         c for c in rule.children if not self.classdb.is_empty(c)
                     )
                     end_labels = tuple(
                         sorted(map(self.classdb.get_label, nonempty_children))
                     )
-                    if end_labels == key[1]:
+                    if (start_label, end_labels) == key:
                         return rule.strategy
                 except StrategyDoesNotApply:
                     pass
