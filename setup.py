c68ea025a1cc210955914f1f00a75a9b224a9cad
Émile Nadeau <nadeau.emile@gmail.com>
2020-08-10 13:28:42 +0000

    [requires.io] dependency update on develop branch (#207)
    * [requires.io] dependency update
    
    * [requires.io] dependency update
    
    Co-authored-by: requires.io <support@requires.io>

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     # Disable formatting until this is solved https://github.com/psf/black/issues/1288
     install_requires=[
         "logzero==1.5.0",
-        "sympy==1.6.1",
+        "sympy==1.6.2",
         "psutil==5.7.2",
         "pympler==0.8",
         "requests==2.24.0",
