8fc46ab61f15945b67ef962a73d5a9b00ebc4b66
Émile Nadeau <nadeau.emile@gmail.com>
2021-04-16 15:55:37 +0000

    fix the test for rule bucket

--- tests/test_forest.py
+++ tests/test_forest.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Union
 
 import pytest
 
-from comb_spec_searcher.rule_db.forest import ForestRuleDB, Function
+from comb_spec_searcher.rule_db.forest import ForestRuleDB, Function, RuleBucket
 
 
 def assert_function_values(
@@ -115,13 +115,13 @@ def test_132_universe_pumping():
     useless.
     """
     ruledb = ForestRuleDB()
-    ruledb.add_rule((0, (1, 2)), (0, 0))  # Point insertion
-    ruledb.add_rule((1, tuple()), tuple())  # Empty verif
-    ruledb.add_rule((2, (3,)), (0,))  # point placement
-    ruledb.add_rule((3, (4,)), (0,))  # row col sep
-    ruledb.add_rule((4, (5, 0, 0)), (0, 1, 1))  # factor
-    ruledb.add_rule((5, tuple()), tuple())  # point verif
-    ruledb.add_rule((2, (6,)), (2,))  # dumb rule
+    ruledb.add_rule((0, (1, 2)), (0, 0), RuleBucket.NORMAL)  # Point insertion
+    ruledb.add_rule((1, tuple()), tuple(), RuleBucket.VERIFICATION)  # Empty verif
+    ruledb.add_rule((2, (3,)), (0,), RuleBucket.NORMAL)  # point placement
+    ruledb.add_rule((3, (4,)), (0,), RuleBucket.NORMAL)  # row col sep
+    ruledb.add_rule((4, (5, 0, 0)), (0, 1, 1), RuleBucket.NORMAL)  # factor
+    ruledb.add_rule((5, tuple()), tuple(), RuleBucket.VERIFICATION)  # point verif
+    ruledb.add_rule((2, (6,)), (2,), RuleBucket.UNDEFINED)  # dumb rule
     assert ruledb.function == {i: None for i in range(6)}
     assert all(ruledb.is_pumping(c) for c in range(6))
     assert not ruledb.is_pumping(6)
@@ -143,64 +143,64 @@ def test_132_universe_pumping_progressive():
     We add rule progressively and make sure the function is always up to date.
     """
     ruledb = ForestRuleDB()
-    ruledb.add_rule((0, (1, 2)), (0, 0))  # Point insertion
+    ruledb.add_rule((0, (1, 2)), (0, 0), RuleBucket.NORMAL)  # Point insertion
     assert ruledb.function == {}
 
-    ruledb.add_rule((1, tuple()), tuple())  # Empty verif
+    ruledb.add_rule((1, tuple()), tuple(), RuleBucket.VERIFICATION)  # Empty verif
     assert ruledb.function == {1: None}
 
-    ruledb.add_rule((2, (3,)), (0,))  # point placement
+    ruledb.add_rule((2, (3,)), (0,), RuleBucket.NORMAL)  # point placement
     assert ruledb.function == {1: None}
 
-    ruledb.add_rule((3, (4,)), (0,))  # row col sep
+    ruledb.add_rule((3, (4,)), (0,), RuleBucket.NORMAL)  # row col sep
     assert ruledb.function == {1: None}
 
-    ruledb.add_rule((5, tuple()), tuple())  # point verif
+    ruledb.add_rule((5, tuple()), tuple(), RuleBucket.VERIFICATION)  # point verif
     assert ruledb.function == {1: None, 5: None}
 
-    ruledb.add_rule((2, (6,)), (-2,))  # dumb rule
+    ruledb.add_rule((2, (6,)), (-2,), RuleBucket.UNDEFINED)  # dumb rule
     assert ruledb.function == {1: None, 5: None}
 
     # This will pump to and 0 a little bit
-    ruledb.add_rule((2, (7,)), (2,))  # dumb rule
+    ruledb.add_rule((2, (7,)), (2,), RuleBucket.UNDEFINED)  # dumb rule
     assert ruledb.function == {0: 2, 1: None, 2: 2, 5: None}
 
-    ruledb.add_rule((4, (5, 0, 0)), (0, 1, 1))  # factor
+    ruledb.add_rule((4, (5, 0, 0)), (0, 1, 1), RuleBucket.NORMAL)  # factor
     assert ruledb.function == {i: None for i in range(6)}
 
 
 def test_universe_not_pumping():
     ruledb = ForestRuleDB()
-    ruledb.add_rule((0, (1, 2)), (0, 0))  # Point insertion
-    ruledb.add_rule((5, tuple()), tuple())  # point verif
-    ruledb.add_rule((2, (3,)), (0,))  # point placement
-    ruledb.add_rule((3, (4,)), (0,))  # row col sep
-    ruledb.add_rule((4, (5, 0, 0)), (0, 1, 1))  # factor
+    ruledb.add_rule((0, (1, 2)), (0, 0), RuleBucket.NORMAL)  # Point insertion
+    ruledb.add_rule((5, tuple()), tuple(), RuleBucket.VERIFICATION)  # point verif
+    ruledb.add_rule((2, (3,)), (0,), RuleBucket.NORMAL)  # point placement
+    ruledb.add_rule((3, (4,)), (0,), RuleBucket.NORMAL)  # row col sep
+    ruledb.add_rule((4, (5, 0, 0)), (0, 1, 1), RuleBucket.NORMAL)  # factor
     assert ruledb.function == {2: 1, 3: 1, 4: 1, 5: None}
 
 
 def test_segmented():
     """The segemented forest."""
     ruledb = ForestRuleDB()
-    ruledb.add_rule((0, (1, 2)), (0, 0))
-    ruledb.add_rule((1, (4, 14)), (0, 0))
-    ruledb.add_rule((2, tuple()), tuple())
+    ruledb.add_rule((0, (1, 2)), (0, 0), RuleBucket.UNDEFINED)
+    ruledb.add_rule((1, (4, 14)), (0, 0), RuleBucket.UNDEFINED)
+    ruledb.add_rule((2, tuple()), tuple(), RuleBucket.UNDEFINED)
     assert ruledb.function == {2: None}
 
-    ruledb.add_rule((3, (16, 5)), (1, 0))
-    ruledb.add_rule((4, tuple()), tuple())
-    ruledb.add_rule((5, tuple()), tuple())
+    ruledb.add_rule((3, (16, 5)), (1, 0), RuleBucket.UNDEFINED)
+    ruledb.add_rule((4, tuple()), tuple(), RuleBucket.UNDEFINED)
+    ruledb.add_rule((5, tuple()), tuple(), RuleBucket.UNDEFINED)
     assert ruledb.function == {2: None, 3: 1, 4: None, 5: None}
 
     # Induced a gap size change
-    ruledb.add_rule((6, (7, 5, 17)), (2, 1, 1))
+    ruledb.add_rule((6, (7, 5, 17)), (2, 1, 1), RuleBucket.UNDEFINED)
     assert ruledb.function == {2: None, 3: 1, 4: None, 5: None, 6: 1}
 
-    ruledb.add_rule((16, (6,)), (0,))
+    ruledb.add_rule((16, (6,)), (0,), RuleBucket.UNDEFINED)
     assert ruledb.function == {2: None, 3: 2, 4: None, 5: None, 6: 1, 16: 1}
 
-    ruledb.add_rule((7, tuple()), tuple())
-    ruledb.add_rule((8, (9, 5)), (1, 0))
+    ruledb.add_rule((7, tuple()), tuple(), RuleBucket.UNDEFINED)
+    ruledb.add_rule((8, (9, 5)), (1, 0), RuleBucket.UNDEFINED)
     assert ruledb.function == {
         2: None,
         3: 2,
@@ -212,11 +212,11 @@ def test_segmented():
         16: 1,
     }
 
-    ruledb.add_rule((12, (20, 5)), (-1, 0))
-    ruledb.add_rule((20, (13,)), (0,))
-    ruledb.add_rule((13, (15, 2, 5)), (-1, 1, 0))
-    ruledb.add_rule((15, (1,)), (0,))
-    ruledb.add_rule((14, (3,)), (0,))
+    ruledb.add_rule((12, (20, 5)), (-1, 0), RuleBucket.UNDEFINED)
+    ruledb.add_rule((20, (13,)), (0,), RuleBucket.UNDEFINED)
+    ruledb.add_rule((13, (15, 2, 5)), (-1, 1, 0), RuleBucket.UNDEFINED)
+    ruledb.add_rule((15, (1,)), (0,), RuleBucket.UNDEFINED)
+    ruledb.add_rule((14, (3,)), (0,), RuleBucket.UNDEFINED)
     assert ruledb.function == {
         0: 2,
         1: 2,
@@ -234,8 +234,8 @@ def test_segmented():
         20: 1,
     }
 
-    ruledb.add_rule((18, (8,)), (0,))
-    ruledb.add_rule((11, (12, 18)), (0, 0))
+    ruledb.add_rule((18, (8,)), (0,), RuleBucket.UNDEFINED)
+    ruledb.add_rule((11, (12, 18)), (0, 0), RuleBucket.UNDEFINED)
     assert ruledb.function == {
         0: 2,
         1: 2,
@@ -254,7 +254,7 @@ def test_segmented():
         20: 1,
     }
 
-    ruledb.add_rule((17, (8,)), (0,))
+    ruledb.add_rule((17, (8,)), (0,), RuleBucket.UNDEFINED)
     assert ruledb.function == {
         0: 3,
         1: 3,
@@ -276,8 +276,8 @@ def test_segmented():
         20: 2,
     }
 
-    ruledb.add_rule((9, (0, 19)), (0, 0))
-    ruledb.add_rule((10, (5, 11)), (0, 1))
+    ruledb.add_rule((9, (0, 19)), (0, 0), RuleBucket.UNDEFINED)
+    ruledb.add_rule((10, (5, 11)), (0, 1), RuleBucket.UNDEFINED)
     assert ruledb.function == {
         0: 3,
         1: 3,
@@ -300,6 +300,6 @@ def test_segmented():
         20: 2,
     }
 
-    ruledb.add_rule((19, (10,)), (0,))
+    ruledb.add_rule((19, (10,)), (0,), RuleBucket.UNDEFINED)
     assert ruledb.function == {i: None for i in range(21)}
     assert all(ruledb.is_pumping(c) for c in range(21))
