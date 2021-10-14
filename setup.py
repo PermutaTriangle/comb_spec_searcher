37b956176358d99a12c1d559425afbccd5282ea3
Émile Nadeau <nadeau.emile@gmail.com>
2021-10-14 10:06:18 +0000

    fix bot mistakes

--- setup.py
+++ setup.py
@@ -8,9 +8,17 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
 setup(
     name="comb_spec_searcher",
-    version="2.3.0",
+    version=get_version("comb_spec_searcher/__init__.py"),
     author="Permuta Triangle",
     author_email="permutatriangle@gmail.com",
     description="A library for performing combinatorial exploration.",
@@ -21,10 +29,10 @@ setup(
         "Source": "https://github.com/PermutaTriangle/comb_spec_searcher",
         "Tracker": ("https://github.com/PermutaTriangle/comb_spec_searcher" "/issues"),
     },
-    packages=find_packages(),
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     package_data={"comb_spec_searcher": ["py.typed"]},
     long_description=read("README.rst"),
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     include_package_data=True,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -32,9 +40,10 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Education",
