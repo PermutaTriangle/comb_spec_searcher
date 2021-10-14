804e82a174558333cc141af16a9432d58d6f45dd
Émile Nadeau <nadeau.emile@gmail.com>
2021-10-14 10:10:58 +0000

    more update

--- setup.py
+++ setup.py
@@ -51,11 +51,11 @@ setup(
     ],
     install_requires=[
         "logzero==1.7.0",
-        "sympy==1.8",
+        "sympy==1.9",
         "psutil==5.8.0",
         "pympler==0.9",
         "requests==2.26.0",
-        "typing-extensions==3.10.0.0",
+        "typing-extensions==3.10.0.2",
         "tabulate==0.8.9",
     ],
 )
